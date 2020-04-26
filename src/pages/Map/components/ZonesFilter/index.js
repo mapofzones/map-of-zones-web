a74f980bc6ba0cb1c59a47b32aa5031819ac4ddd
Maksim Tolochko <maksim.tolo@gmail.com>
2020-04-26 17:09:00 +0300

    Fix "back-to-map" button

--- src/pages/Map/components/ZonesFilter/index.js
+++ src/pages/Map/components/ZonesFilter/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
+import { FormattedMessage } from 'react-intl';
+
+import { ReactComponent as ArrowTop } from 'assets/images/arrow-top.svg';
 
 import PeriodSwitcher from '../PeriodSwitcher';
 
@@ -8,11 +11,31 @@ import styles from './index.module.css';
 
 const cx = classNames.bind(styles);
 
-function ZonesFilter({ hours, setPeriod, className }) {
+function ZonesFilter({
+  hours,
+  setPeriod,
+  className,
+  isTableOpened,
+  toggleTableOpen,
+}) {
   return (
     <div className={cx('container', className)}>
       <div />
-      <PeriodSwitcher hours={hours} onChange={setPeriod} />
+      {isTableOpened ? (
+        <button
+          type="button"
+          className={cx('closeTableButton')}
+          onClick={toggleTableOpen}
+        >
+          <ArrowTop className={cx('arrowTop')} />
+          <FormattedMessage
+            id="back-to-map-title"
+            defaultMessage="Back to Map"
+          />
+        </button>
+      ) : (
+        <PeriodSwitcher hours={hours} onChange={setPeriod} />
+      )}
     </div>
   );
 }
@@ -21,6 +44,8 @@ ZonesFilter.propTypes = {
   hours: PropTypes.number,
   setPeriod: PropTypes.func,
   className: PropTypes.string,
+  isTableOpened: PropTypes.bool,
+  toggleTableOpen: PropTypes.func,
 };
 
 export default ZonesFilter;
