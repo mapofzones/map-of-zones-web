b9bceac77abf3ecde2374710c3b3de195ad65405
Maksim Tolochko <maksim.tolo@gmail.com>
2020-04-27 01:34:40 +0300

    Add onSortChange callback

--- src/pages/Map/components/Leaderboard/index.js
+++ src/pages/Map/components/Leaderboard/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classNames from 'classnames/bind';
 import { useTable, useSortBy } from 'react-table';
 
@@ -9,13 +9,14 @@ import styles from './index.module.css';
 
 const cx = classNames.bind(styles);
 
-function Leaderboard({ data, toggleTableOpen }) {
+function Leaderboard({ data, toggleTableOpen, onSortChange }) {
   const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
     rows,
     prepareRow,
+    state,
   } = useTable(
     {
       columns,
@@ -24,7 +25,11 @@ function Leaderboard({ data, toggleTableOpen }) {
     useSortBy,
   );
 
-  const headerWithSort = (id) => {
+  const sortedColumn = state?.sortBy?.[0]?.id;
+
+  useEffect(() => onSortChange(sortedColumn), [sortedColumn, onSortChange]);
+
+  const headerWithSort = id => {
     switch (id) {
       case 'totalTxs':
       case 'totalIbcTxs':
@@ -35,7 +40,7 @@ function Leaderboard({ data, toggleTableOpen }) {
         return false;
     }
   };
-  const headerWithExplanation = (id) => {
+  const headerWithExplanation = id => {
     switch (id) {
       case 'totalTxs':
       case 'totalIbcTxs':
@@ -48,43 +53,48 @@ function Leaderboard({ data, toggleTableOpen }) {
     }
   };
 
-  const renderCell = (cell) => {
+  const renderCell = cell => {
     switch (cell.column.id) {
       case 'txsActivity':
         return cell.render('Cell');
       case 'name':
         return (
           <div className={cx('cell-container')}>
-            <span className={cx('text-container')}>
-              {cell.render('Cell')}
-            </span>
-            <span className={cx('position-shift',
-              {negative: cell.row.id%20 - 10 < 0},
-              {new: cell.row.id%20 - 10 === 0})}>
-
-              {cell.row.id%20 - 10 !== 0 ?
-                cell.row.id%20 - 10 :
+            <span className={cx('text-container')}>{cell.render('Cell')}</span>
+            <span
+              className={cx(
+                'position-shift',
+                { negative: (cell.row.id % 20) - 10 < 0 },
+                { new: (cell.row.id % 20) - 10 === 0 },
+              )}
+            >
+              {(cell.row.id % 20) - 10 !== 0 ? (
+                (cell.row.id % 20) - 10
+              ) : (
                 <div>NEW</div>
-              }
+              )}
             </span>
           </div>
         );
-
       default:
-        return <span className={cx('text-container')}>
-                 {cell.render('Cell')}
-
-                {headerWithSort(cell.column.id) &&
-                <div className={cx('shift-tooltip', {negative:cell.value - cell.row.values.ibcReceived < 0})}>
-                  {cell.value - cell.row.values.ibcReceived > 0
-                    ? "+" + cell.value - cell.row.values.ibcReceived
-                    : cell.value - cell.row.values.ibcReceived}
-                </div>}
-               </span>
+        return (
+          <span className={cx('text-container')}>
+            {cell.render('Cell')}
+            {headerWithSort(cell.column.id) && (
+              <div
+                className={cx('shift-tooltip', {
+                  negative: cell.value - cell.row.values.ibcReceived < 0,
+                })}
+              >
+                {cell.value - cell.row.values.ibcReceived > 0
+                  ? '+' + cell.value - cell.row.values.ibcReceived
+                  : cell.value - cell.row.values.ibcReceived}
+              </div>
+            )}
+          </span>
+        );
     }
   };
-
-  const firstPageRows = rows.slice(0, 20);
 
   return (
     <div className={cx('table-container')}>
@@ -93,31 +103,33 @@ function Leaderboard({ data, toggleTableOpen }) {
           {headerGroups.map(headerGroup => (
             <tr {...headerGroup.getHeaderGroupProps()}>
               {headerGroup.headers.map(column => (
-
                 <th
                   {...column.getHeaderProps(
                     headerWithSort(column.id)
                       ? column.getSortByToggleProps()
                       : undefined,
                   )}
-                  className={cx('header', column.id, {sortedColumn: column.isSorted})}
+                  className={cx('header', column.id, {
+                    sortedColumn: column.isSorted,
+                  })}
                 >
                   <div className={cx('header-container')}>
-                    <div className={cx('IBC-circle',  column.id)}/>
-
+                    <div className={cx('IBC-circle', column.id)} />
                     {column.render('Header')}
-
-                    {headerWithExplanation(column.id) &&
-                    <div className={cx('explanation-icon')}>
-                      ?
-                      <div className={cx('explanation-tooltip',
-                        {centerPos:column.id === 'channels' || column.id === 'ibcReceived'})}>
-                       {column.descr}
+                    {headerWithExplanation(column.id) && (
+                      <div className={cx('explanation-icon')}>
+                        ?
+                        <div
+                          className={cx('explanation-tooltip', {
+                            centerPos:
+                              column.id === 'channels' ||
+                              column.id === 'ibcReceived',
+                          })}
+                        >
+                          {column.descr}
+                        </div>
                       </div>
-                    </div>
-                    }
-
-                    {/*{column.isSorted ? (*/}
+                    )}
                     {headerWithSort(column.id) ? (
                       <div className={cx('sort-arrow-wrapper')}>
                         <ArrowDown />
@@ -127,28 +139,32 @@ function Leaderboard({ data, toggleTableOpen }) {
                       ''
                     )}
                   </div>
-
                 </th>
               ))}
             </tr>
           ))}
         </thead>
         <tbody {...getTableBodyProps()}>
-          {firstPageRows.map((row, i) => {
+          {rows.map((row, i) => {
             prepareRow(row);
             return (
-              <tr {...row.getRowProps()} className={cx('row')}
-                  onClick={()=>toggleTableOpen('open')}>
+              <tr
+                {...row.getRowProps()}
+                className={cx('row')}
+                onClick={() => toggleTableOpen('open')}
+              >
                 {row.cells.map(cell => {
                   return (
                     <td
-                      onClick={()=>{console.log(cell)}}
+                      onClick={() => {
+                        console.log(cell);
+                      }}
                       {...cell.getCellProps()}
-                      className={cx('cell', cell.column.id, {sortedColumn: cell.column.isSorted})}
+                      className={cx('cell', cell.column.id, {
+                        sortedColumn: cell.column.isSorted,
+                      })}
                     >
-
                       {renderCell(cell)}
-
                     </td>
                   );
                 })}
