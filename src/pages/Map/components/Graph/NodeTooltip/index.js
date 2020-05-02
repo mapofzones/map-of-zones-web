0f4dec469f7dc9aa35bd3eb6e200f064c602ffe7
agapotov <kagapotov@mail.ru>
2020-05-02 15:30:39 +0300

    #43 (change tooltip position)

--- src/pages/Map/components/Graph/NodeTooltip/index.js
+++ src/pages/Map/components/Graph/NodeTooltip/index.js
@@ -21,8 +21,8 @@ function NodeTooltip({ node, period }) {
       className={cx('node-custom-tooltip', { active: isActive })}
       style={{
         borderTop: `4px solid ${node.color}`,
-        left: mapTooltip.style.left,
-        top: mapTooltip.style.top,
+        left: (parseInt(mapTooltip.style.left) + 15) + 'px',
+        top: (parseInt(mapTooltip.style.top) + 15) + 'px',
       }}
     >
       <div className={cx('header-row')}>
