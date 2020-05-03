5fdd817ce855eccadea66c9e273d1e2f330b253b
Maksim Tolochko <maksim.tolo@gmail.com>
2020-05-03 19:21:11 +0300

    Add url state management

---
+++ src/pages/Map/hooks/period.js
@@ -0,0 +1,40 @@
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import { parse, stringify } from 'querystringify';
+
+import { PERIODS } from '../components/PeriodSwitcher';
+
+export const usePeriodSelector = () => {
+  const history = useHistory();
+  const location = useLocation();
+  const [selectedPeriod, setPeriod] = useState(PERIODS[0]);
+  const updatePeriod = useCallback(
+    period => {
+      const search = parse(location.search);
+
+      if (period && period.hours) {
+        search.period = period.hours.toString(10);
+      } else {
+        delete search.period;
+      }
+
+      history.push(`?${stringify(search)}`);
+    },
+    [history, location.search],
+  );
+  const period = useMemo(() => parseInt(parse(location.search).period, 10), [
+    location.search,
+  ]);
+
+  useEffect(() => {
+    if (period) {
+      const matchedPeriod = PERIODS.find(({ hours }) => period === hours);
+
+      if (matchedPeriod) {
+        setPeriod(matchedPeriod);
+      }
+    }
+  }, [period, setPeriod]);
+
+  return [selectedPeriod, updatePeriod];
+};
