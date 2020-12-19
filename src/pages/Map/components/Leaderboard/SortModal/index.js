11dd4cf38c4eaf766635d233e3ed79d20027a659
Maksim Tolochko <maksim.tolo@gmail.com>
2020-12-19 16:44:42 +0800

    Update sort modal

--- src/pages/Map/components/Leaderboard/SortModal/index.js
+++ src/pages/Map/components/Leaderboard/SortModal/index.js
@@ -1,10 +1,9 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import { FormattedMessage } from 'react-intl';
 import Modal from 'react-modal';
 import WheelPicker from '@maksim-tolo/react-wheel-picker';
-import debounce from 'lodash.debounce';
 
 import styles from './index.module.css';
 
@@ -17,14 +16,17 @@ function SortModal({
   selectedIndex,
   updateSelection,
 }) {
+  const [index, setIndex] = useState(selectedIndex);
   const onChange = useCallback(
-    debounce(
-      selected =>
-        updateSelection(data.findIndex(({ id }) => id === selected.id)),
-      500,
-    ),
-    [updateSelection, data],
+    selected => setIndex(data.findIndex(({ id }) => id === selected.id)),
+    [setIndex, data],
   );
+  const applyChanges = useCallback(() => updateSelection(index), [
+    updateSelection,
+    index,
+  ]);
+
+  useEffect(() => setIndex(selectedIndex), [selectedIndex]);
 
   if (!data) {
     return null;
@@ -37,26 +39,35 @@ function SortModal({
       overlayClassName={cx('overlay')}
       className={cx('content')}
     >
-      <div className={cx('selectedValueContainer')}>
-        <div>
-          <FormattedMessage
-            id="sort-modal-selected-value-title"
-            defaultMessage="Show indicator"
-          />
+      <div className={cx('container')}>
+        <div className={cx('selectedValueContainer')}>
+          <div>
+            <FormattedMessage
+              id="sort-modal-selected-value-title"
+              defaultMessage="Show indicator"
+            />
+          </div>
+          <div className={cx('selectedValue')}>{data[index].value}</div>
         </div>
-        <div className={cx('selectedValue')}>{data[selectedIndex].value}</div>
+        <WheelPicker
+          data={data}
+          onChange={onChange}
+          height={150}
+          itemHeight={32}
+          selectedID={data[index].id}
+          color="#ccc"
+          activeColor="#333"
+          backgroundColor="#fff"
+          shadowColor="#fff"
+        />
       </div>
-      <WheelPicker
-        data={data}
-        onChange={onChange}
-        height={150}
-        itemHeight={32}
-        selectedID={data[selectedIndex].id}
-        color="#ccc"
-        activeColor="#333"
-        backgroundColor="#fff"
-        shadowColor="#fff"
-      />
+      <button
+        type="button"
+        className={cx('applyButton')}
+        onClick={applyChanges}
+      >
+        <FormattedMessage id="apply-button-title" defaultMessage="Apply" />
+      </button>
     </Modal>
   );
 }
