25a8fb85cac74ca1f8895cf7139c9a183888def0
Maksim Tolochko <maksim.tolo@gmail.com>
2021-02-22 21:49:11 +0800

    Add 3d map

---
+++ src/pages/Map/components/Graph/2d.js
@@ -0,0 +1,329 @@
+import React, { useRef, useEffect, useState, useCallback } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import ForceGraph2D from 'react-force-graph-2d';
+import { FormattedMessage } from 'react-intl';
+import { forceCollide } from 'd3-force-3d';
+
+import { trackEvent } from 'common/helper';
+
+import { ReactComponent as FilterIcon } from 'assets/images/filter.svg';
+import { ReactComponent as MinusSign } from 'assets/images/minus.svg';
+import { ReactComponent as PlusSign } from 'assets/images/plus.svg';
+import { ReactComponent as FullScreenIcon } from 'assets/images/fulll-screen-icon.svg';
+import { ReactComponent as CollapseScreenIcon } from 'assets/images/collapse-screen-icon.svg';
+import { ReactComponent as CloseIcon } from 'assets/images/close-icon.svg';
+import { ReactComponent as TgShareLogo } from 'assets/images/tg-share.svg';
+import { ReactComponent as TwitterShareLogo } from 'assets/images/twitter-share.svg';
+
+import {
+  useNodeCanvasObject,
+  useLinkCanvasObject,
+  useFocusedNodeNeighbors,
+  useTwitterShareText,
+  useTelegramShareText,
+  // useRenderFrame,
+} from './hooks';
+import NodeTooltip from './Tooltips/NodeTooltip';
+import LinkTooltip from './Tooltips/LinkTooltip';
+import ZonesColorDescriptor from './ZonesColorDescriptor';
+import ZonesFilter from '../ZonesFilter';
+
+// import NodeModal from './Modal/NodeModal';
+
+import styles from './index.module.css';
+
+const cx = classNames.bind(styles);
+
+const NODE_REL_SIZE = 4;
+
+function Graph({
+  data,
+  isBlurred,
+  period,
+  zoneWeightAccessor,
+  mapOpened,
+  toggleMapOpen,
+  onNodeFocus,
+  focusedNode,
+  setFilter,
+  currentFilter,
+}) {
+  const [hoveredNode, setHoveredNode] = useState(null);
+  // const [clickedNode, setClickedNode] = useState(null);
+  const [hoveredLink, setHoveredLink] = useState(null);
+  const [currentZoom, updateZoom] = useState(1);
+  const [showFilter, setShowFilter] = useState(false);
+  // const [isModalOpened, setModalOpened] = useState(false);
+  const fgRef = useRef();
+
+  useEffect(() => {
+    const fg = fgRef.current;
+
+    // links
+    fg.d3Force('link').distance(() => Math.random() * 100 + 150);
+
+    // charge
+    fg.d3Force('charge').distanceMax(200);
+  }, []);
+
+  useEffect(() => {
+    // collision
+    const collide = forceCollide(node => {
+      return (
+        Math.sqrt(Math.max(0, node[zoneWeightAccessor] || 1)) * NODE_REL_SIZE +
+        2
+      );
+    });
+    fgRef.current.d3Force('collide', collide);
+  }, [zoneWeightAccessor]);
+
+  useEffect(() => {
+    if (mapOpened) {
+      window.scrollTo(0, 0);
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }, [mapOpened]);
+
+  const zoom = useCallback(
+    val => {
+      fgRef.current.zoom(val, 500);
+      updateZoom(val);
+    },
+    [updateZoom],
+  );
+
+  useEffect(() => {
+    if (focusedNode) {
+      fgRef.current.centerAt(focusedNode.x, focusedNode.y, 500);
+      zoom(2);
+    } else {
+      zoom(1);
+    }
+  }, [focusedNode, zoom]);
+
+  const zoomIn = useCallback(() => {
+    zoom(currentZoom * 2);
+    trackEvent({ category: 'Map', action: 'zoom', label: 'in' });
+  }, [currentZoom, zoom]);
+  const zoomOut = useCallback(() => {
+    zoom(currentZoom / 2);
+    trackEvent({ category: 'Map', action: 'zoom', label: 'out' });
+  }, [currentZoom, zoom]);
+  const onNodeHover = useCallback(node => setHoveredNode(node), [
+    setHoveredNode,
+  ]);
+  const onLinkHover = useCallback(link => setHoveredLink(link), [
+    setHoveredLink,
+  ]);
+  const toggleFilter = useCallback(() => setShowFilter(!showFilter), [
+    setShowFilter,
+    showFilter,
+  ]);
+  const clearNodeFocus = useCallback(() => {
+    if (focusedNode) {
+      onNodeFocus(null);
+    }
+  }, [focusedNode, onNodeFocus]);
+  const focusedNodeNeighbors = useFocusedNodeNeighbors(focusedNode, data.graph);
+  const onNodeClick = useCallback(
+    node => {
+      if (!focusedNode || focusedNodeNeighbors.includes(node)) {
+        onNodeFocus(node);
+        trackEvent({
+          category: 'Map',
+          action: 'select zone',
+          label: node.name,
+          extra: { period: period?.rawText },
+        });
+        // setModalOpened(true);
+        // setClickedNode(node);
+      } else {
+        clearNodeFocus();
+      }
+    },
+    [focusedNode, onNodeFocus, focusedNodeNeighbors, clearNodeFocus, period],
+  );
+
+  const nodeCanvasObject = useNodeCanvasObject(
+    zoneWeightAccessor,
+    focusedNode,
+    focusedNodeNeighbors,
+    NODE_REL_SIZE,
+  );
+  const applyFilter = useCallback(
+    filter => {
+      clearNodeFocus();
+      setFilter(filter);
+      setShowFilter(false);
+    },
+    [setShowFilter, setFilter, clearNodeFocus],
+  );
+  const linkCanvasObject = useLinkCanvasObject(focusedNode);
+  const twitterShareText = useTwitterShareText(focusedNode, period);
+  const telegramShareText = useTelegramShareText(focusedNode, period);
+  // const onRenderFrame = useRenderFrame();
+
+  return (
+    <div>
+      <div className={cx('container', { blurred: isBlurred })}>
+        <ForceGraph2D
+          ref={fgRef}
+          height={mapOpened ? document.documentElement.clientHeight : 500}
+          nodeRelSize={NODE_REL_SIZE}
+          nodeVal={zoneWeightAccessor}
+          nodeColor="color"
+          nodeLabel={null}
+          graphData={data}
+          onNodeHover={onNodeHover}
+          nodeCanvasObject={nodeCanvasObject}
+          linkCanvasObject={linkCanvasObject}
+          onNodeClick={onNodeClick}
+          onLinkHover={onLinkHover}
+          d3AlphaDecay={0.02}
+          d3VelocityDecay={0.3}
+          // onRenderFramePost={onRenderFrame}
+          onNodeDragEnd={() =>
+            trackEvent({
+              category: 'Map',
+              action: 'drag zone',
+            })
+          }
+          onBackgroundClick={clearNodeFocus}
+        />
+        <ZonesColorDescriptor className={cx('zonesColorDescriptor')} />
+        <div className={cx('buttonsContainer', 'zoomButtonsContainer')}>
+          <div className={cx('zonesFilterContainer')}>
+            {(currentFilter?.sortOrder || currentFilter?.trendLine) && (
+              <div className={cx('dot')} />
+            )}
+            <button
+              type="button"
+              onClick={toggleFilter}
+              className={cx('roundButton', 'filterButton')}
+            >
+              <FilterIcon />
+            </button>
+          </div>
+          <button type="button" onClick={zoomIn} className={cx('roundButton')}>
+            <PlusSign />
+          </button>
+          <button type="button" onClick={zoomOut} className={cx('roundButton')}>
+            <MinusSign />
+          </button>
+          {(!mapOpened && (
+            <button
+              type="button"
+              onClick={() => toggleMapOpen('open')}
+              className={cx('roundButton', 'fullScreenButton')}
+            >
+              <FullScreenIcon />
+            </button>
+          )) || (
+            <button
+              type="button"
+              onClick={toggleMapOpen}
+              className={cx('roundButton', 'fullScreenButton')}
+            >
+              <CollapseScreenIcon />
+            </button>
+          )}
+        </div>
+        {!!focusedNode && (
+          <div className={cx('buttonsContainer', 'shareButtonsContainer')}>
+            <div className={cx('shareTitle')}>
+              <FormattedMessage id="share" defaultMessage="Share" />
+            </div>
+            <a
+              onClick={() =>
+                trackEvent({
+                  category: 'Map',
+                  action: 'telegram share',
+                  label: focusedNode.name,
+                })
+              }
+              href={telegramShareText}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={cx('roundButton')}
+            >
+              <TgShareLogo />
+            </a>
+            <a
+              onClick={() =>
+                trackEvent({
+                  category: 'Map',
+                  action: 'twitter share',
+                  label: focusedNode.name,
+                  extra: { period: period?.rawText },
+                })
+              }
+              href={twitterShareText}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={cx('roundButton')}
+            >
+              <TwitterShareLogo />
+            </a>
+          </div>
+        )}
+        {!!focusedNode && (
+          <button
+            type="button"
+            onClick={clearNodeFocus}
+            className={cx('roundButton', 'closeButton')}
+          >
+            <CloseIcon />
+          </button>
+        )}
+      </div>
+      {hoveredNode && <NodeTooltip node={hoveredNode} period={period.name} />}
+      {hoveredLink && <LinkTooltip link={hoveredLink} period={period.name} />}
+      <ZonesFilter
+        onRequestClose={toggleFilter}
+        isOpen={showFilter}
+        applyFilter={applyFilter}
+        currentFilter={currentFilter}
+      />
+      {/*{clickedNode && (*/}
+      {/*  <NodeModal*/}
+      {/*    isOpen={isModalOpened}*/}
+      {/*    onRequestClose={() => setModalOpened(false)}*/}
+      {/*    node={clickedNode}*/}
+      {/*    period={period.name}*/}
+      {/*  />*/}
+      {/*)}*/}
+    </div>
+  );
+}
+
+Graph.propTypes = {
+  data: PropTypes.shape({
+    nodes: PropTypes.array,
+    links: PropTypes.array,
+    graph: PropTypes.object,
+  }),
+  isBlurred: PropTypes.string,
+  period: PropTypes.shape({
+    hours: PropTypes.number,
+    step: PropTypes.number,
+    name: PropTypes.node,
+    rawText: PropTypes.string,
+  }),
+  zoneWeightAccessor: PropTypes.string,
+  mapOpened: PropTypes.bool,
+  toggleMapOpen: PropTypes.func,
+  onNodeFocus: PropTypes.func,
+  setFilter: PropTypes.func,
+  currentFilter: PropTypes.object,
+  focusedNode: PropTypes.object,
+};
+
+Graph.defaultProps = {
+  zoneWeightAccessor: 'ibcTxsWeight',
+  onNodeFocus: () => {},
+};
+
+export default Graph;
