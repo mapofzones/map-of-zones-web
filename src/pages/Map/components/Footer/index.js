6ae75e8fa1bb6855998f8b5dc206df0ebc6e6c16
adziki <aliaksandr.dziki@e-hps.com>
2021-09-14 19:25:32 +0200

    Add structure for Contact Form

--- src/pages/Map/components/Footer/index.js
+++ src/pages/Map/components/Footer/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames/bind';
 import { FormattedMessage } from 'react-intl';
 
@@ -9,138 +9,142 @@ import { ReactComponent as TgLogo } from 'assets/images/tg-logo.svg';
 import { ReactComponent as TwitterLogo } from 'assets/images/twitter-logo.svg';
 
 import styles from './index.module.css';
+import ContactForm from '../ContactForm';
 
 const cx = classNames.bind(styles);
 
 function Footer() {
+  const [showContactForm, setShowContactForm] = useState(false);
+
+  const handleCloseContactForm = () => setShowContactForm(false);
+  const handleShowContactForm = () => setShowContactForm(true);
+
   return (
-    <div className={cx('container')}>
-      <div>
-        <FormattedMessage
-          id="developers-links"
-          defaultMessage="Map of Zones by {bitquasarLink} & {ztakeLink}"
-          values={{
-            bitquasarLink: (
-              <a
-                href="https://bitquasar.com"
-                target="_blank"
-                rel="noopener noreferrer"
-                className={cx('link')}
-                onClick={() =>
-                  trackEvent({
-                    category: 'Footer',
-                    action: 'bitquasar link click',
-                  })
-                }
-              >
-                Bitquasar.com
-              </a>
-            ),
-            ztakeLink: (
-              <a
-                href="https://ztake.org"
-                target="_blank"
-                rel="noopener noreferrer"
-                className={cx('link')}
-                onClick={() =>
-                  trackEvent({
-                    category: 'Footer',
-                    action: 'ztake link click',
-                  })
-                }
-              >
-                Ztake.org
-              </a>
-            ),
-          }}
-        />
+    <>
+      <ContactForm
+        isOpen={showContactForm}
+        onRequestClose={handleCloseContactForm}
+      />
+      <div className={cx('container')}>
+        <div>
+          <FormattedMessage
+            id="developers-links"
+            defaultMessage="Map of Zones by {bitquasarLink} & {ztakeLink}"
+            values={{
+              bitquasarLink: (
+                <a
+                  href="https://bitquasar.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={cx('link')}
+                  onClick={() =>
+                    trackEvent({
+                      category: 'Footer',
+                      action: 'bitquasar link click',
+                    })
+                  }
+                >
+                  Bitquasar.com
+                </a>
+              ),
+              ztakeLink: (
+                <a
+                  href="https://ztake.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={cx('link')}
+                  onClick={() =>
+                    trackEvent({
+                      category: 'Footer',
+                      action: 'ztake link click',
+                    })
+                  }
+                >
+                  Ztake.org
+                </a>
+              ),
+            }}
+          />
+        </div>
+        <div className={cx('socialContainer')}>
+          <a
+            href="https://github.com/mapofzones"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={cx('link', 'socialLink')}
+            onClick={() =>
+              trackEvent({
+                category: 'Footer',
+                action: 'github link click',
+              })
+            }
+          >
+            <GitHubLogo className={cx('socialIcon')} />
+            <span className={cx('socialName')}>GitHub</span>
+          </a>
+          <a
+            href="https://twitter.com/mapofzones"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={cx('link', 'socialLink')}
+            onClick={() =>
+              trackEvent({
+                category: 'Footer',
+                action: 'twitter link click',
+              })
+            }
+          >
+            <TwitterLogo className={cx('socialIcon')} />
+            <span className={cx('socialName')}>Twitter</span>
+          </a>
+          <a
+            href="https://t.me/MapOfZones"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={cx('link', 'socialLink')}
+            onClick={() =>
+              trackEvent({
+                category: 'Footer',
+                action: 'telegram link click',
+              })
+            }
+          >
+            <TgLogo className={cx('socialIcon')} />
+            <span className={cx('socialName')}>Telegram</span>
+          </a>
+          <a
+            href="mailto:hi@ztake.org"
+            className={cx('link', 'socialLink', 'contactUsLink')}
+            onClick={() =>
+              trackEvent({
+                category: 'Footer',
+                action: 'take email click',
+              })
+            }
+          >
+            <span className={cx('contactUsTitle')}>
+              <FormattedMessage
+                id="contact-us-title"
+                defaultMessage="Contact us: "
+              />
+            </span>
+            <span className={cx('socialName')}>support@mapofzones.com</span>
+          </a>
+          <div
+            className={cx('link', 'socialLink', 'contactUsLink')}
+            onClick={() => handleShowContactForm()}
+          >
+            <span className={cx('socialName')}>not listed?</span>
+          </div>
+        </div>
+        <div className={cx('socialMediaTitle')}>
+          <FormattedMessage
+            id="social-media-title"
+            defaultMessage="We on social media"
+          />
+        </div>
       </div>
-      <div className={cx('socialContainer')}>
-        <a
-          href="https://github.com/mapofzones"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={cx('link', 'socialLink')}
-          onClick={() =>
-            trackEvent({
-              category: 'Footer',
-              action: 'github link click',
-            })
-          }
-        >
-          <GitHubLogo className={cx('socialIcon')} />
-          <span className={cx('socialName')}>GitHub</span>
-        </a>
-        <a
-          href="https://twitter.com/mapofzones"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={cx('link', 'socialLink')}
-          onClick={() =>
-            trackEvent({
-              category: 'Footer',
-              action: 'twitter link click',
-            })
-          }
-        >
-          <TwitterLogo className={cx('socialIcon')} />
-          <span className={cx('socialName')}>Twitter</span>
-        </a>
-        <a
-          href="https://t.me/MapOfZones"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={cx('link', 'socialLink')}
-          onClick={() =>
-            trackEvent({
-              category: 'Footer',
-              action: 'telegram link click',
-            })
-          }
-        >
-          <TgLogo className={cx('socialIcon')} />
-          <span className={cx('socialName')}>Telegram</span>
-        </a>
-        <a
-          href="mailto:hi@ztake.org"
-          className={cx('link', 'socialLink', 'contactUsLink')}
-          onClick={() =>
-            trackEvent({
-              category: 'Footer',
-              action: 'take email click',
-            })
-          }
-        >
-          <span className={cx('contactUsTitle')}>
-            <FormattedMessage
-              id="contact-us-title"
-              defaultMessage="Contact us: "
-            />
-          </span>
-          <span className={cx('socialName')}>support@mapofzones.com</span>
-        </a>
-        <a
-          href="https://docs.google.com/forms/d/e/1FAIpQLSczp-YbmECgX3_DsycgHVsZiJlQZLhB46jFfchQKPxiwFR4ig/viewform?usp=sf_link"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={cx('link', 'socialLink', 'contactUsLink')}
-          onClick={() =>
-            trackEvent({
-              category: 'Footer',
-              action: 'contact us click',
-            })
-          }
-        >
-          <span className={cx('socialName')}>not listed?</span>
-        </a>
-      </div>
-      <div className={cx('socialMediaTitle')}>
-        <FormattedMessage
-          id="social-media-title"
-          defaultMessage="We on social media"
-        />
-      </div>
-    </div>
+    </>
   );
 }
 
