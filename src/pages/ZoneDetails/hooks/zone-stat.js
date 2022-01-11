52240e8779510268d8ba7c92c6294b1eb85fd3d4
Maksim Tolochko <maksim.tolo@gmail.com>
2022-01-11 23:56:30 +0800

    Add channels info

--- src/pages/ZoneDetails/hooks/zone-stat.js
+++ src/pages/ZoneDetails/hooks/zone-stat.js
@@ -4,7 +4,7 @@ import gql from 'graphql-tag';
 import { useRealtimeQuery } from 'common/hooks';
 
 const CHANNEL_GROUP_STAT_FRAGMENT = gql`
-  fragment stat on ft_channel_group_stats {
+  fragment group_stats on ft_channel_group_stats {
     is_zone_up_to_date
     zone_counterparty
     zone_counterparty_label_url
@@ -23,11 +23,29 @@ const CHANNEL_GROUP_STAT_FRAGMENT = gql`
   }
 `;
 
-const SOURCE_ZONE_INFO = `
-  ft_channels_stats(where: { zone: { _eq: $source } }, limit: 1) {
+// TODO: Do we really need client_id
+const CHANNELS_STAT_FRAGMENT = gql`
+  fragment channels_stats on ft_channels_stats {
     zone_website
     zone_readable_name
     zone_label_url
+    ibc_cashflow_in
+    ibc_cashflow_in_diff
+    ibc_cashflow_out
+    ibc_cashflow_out_diff
+    ibc_tx
+    ibc_tx_diff
+    ibc_tx_failed
+    ibc_tx_failed_diff
+    ibc_tx_success_rate
+    ibc_tx_success_rate_diff
+    channel_id
+    zone_counterparty
+    zone_counterparty_channel_id
+    zone_counterparty_readable_name
+    client_id
+    connection_id
+    is_opened
   }
 `; // TODO: add is_zone_up_to_date
 
@@ -36,11 +54,16 @@ const CHANNEL_GROUP_STAT_QUERY = gql`
     ft_channel_group_stats(
       where: { zone: { _eq: $source }, timeframe: { _eq: $period } }
     ) {
-      ...stat
+      ...group_stats
     }
-    ${SOURCE_ZONE_INFO}
+    ft_channels_stats(
+      where: { zone: { _eq: $source }, timeframe: { _eq: $period } }
+    ) {
+      ...channels_stats
+    }
   }
   ${CHANNEL_GROUP_STAT_FRAGMENT}
+  ${CHANNELS_STAT_FRAGMENT}
 `;
 
 const CHANNEL_GROUP_STAT_SUBSCRIPTION = gql`
@@ -48,11 +71,16 @@ const CHANNEL_GROUP_STAT_SUBSCRIPTION = gql`
     ft_channel_group_stats(
       where: { zone: { _eq: $source }, timeframe: { _eq: $period } }
     ) {
-      ...stat
+      ...group_stats
     }
-    ${SOURCE_ZONE_INFO}
+    ft_channels_stats(
+      where: { zone: { _eq: $source }, timeframe: { _eq: $period } }
+    ) {
+      ...channels_stats
+    }
   }
   ${CHANNEL_GROUP_STAT_FRAGMENT}
+  ${CHANNELS_STAT_FRAGMENT}
 `;
 
 const CHANNEL_GROUP_STAT_QUERY_ONLY_MAINNET = gql`
@@ -64,11 +92,20 @@ const CHANNEL_GROUP_STAT_QUERY_ONLY_MAINNET = gql`
         is_zone_counterparty_mainnet: { _eq: true }
       }
     ) {
-      ...stat
+      ...group_stats
     }
-    ${SOURCE_ZONE_INFO}
+    ft_channels_stats(
+      where: {
+        zone: { _eq: $source }
+        timeframe: { _eq: $period }
+        is_zone_counterparty_mainnet: { _eq: true }
+      }
+    ) {
+      ...channels_stats
+    }
   }
   ${CHANNEL_GROUP_STAT_FRAGMENT}
+  ${CHANNELS_STAT_FRAGMENT}
 `;
 
 const CHANNEL_GROUP_STAT_SUBSCRIPTION_ONLY_MAINNET = gql`
@@ -80,16 +117,26 @@ const CHANNEL_GROUP_STAT_SUBSCRIPTION_ONLY_MAINNET = gql`
         is_zone_counterparty_mainnet: { _eq: true }
       }
     ) {
-      ...stat
+      ...group_stats
     }
-    ${SOURCE_ZONE_INFO}
+    ft_channels_stats(
+      where: {
+        zone: { _eq: $source }
+        timeframe: { _eq: $period }
+        is_zone_counterparty_mainnet: { _eq: true }
+      }
+    ) {
+      ...channels_stats
+    }
   }
   ${CHANNEL_GROUP_STAT_FRAGMENT}
+  ${CHANNELS_STAT_FRAGMENT}
 `;
 
 const transform = data => {
-  const channels = data?.ft_channel_group_stats;
-  const sourceZone = data?.ft_channels_stats?.[0];
+  const channelsGroup = data?.ft_channel_group_stats;
+  const channels = data?.ft_channels_stats;
+  const sourceZone = channels?.[0]; // TODO: Use a separate query for the source zone
   const sourceZoneFormatted = sourceZone
     ? {
         website: sourceZone.zone_website,
@@ -98,8 +145,8 @@ const transform = data => {
       }
     : null;
 
-  const channelsFormatted = channels
-    ? channels.map(
+  const channelsGroupFormatted = channelsGroup
+    ? channelsGroup.map(
         ({
           is_zone_up_to_date, // Do we really need it here?
           zone_counterparty,
@@ -117,10 +164,31 @@ const transform = data => {
           ibc_tx_failed,
           ibc_tx_failed_diff,
         }) => {
+          const channelsFormatted = (channels || [])
+            .filter(channel => channel.zone_counterparty === zone_counterparty)
+            .map(channel => ({
+              id: channel.connection_id,
+              name: channel.connection_id,
+              zoneLabelUrl: channel.zone_label_url,
+              volumeIn: channel.ibc_cashflow_in,
+              volumeInDiff: channel.ibc_cashflow_in_diff,
+              volumeOut: channel.ibc_cashflow_out,
+              volumeOutDiff: channel.ibc_cashflow_out_diff,
+              ibcTxSuccess: channel.ibc_tx,
+              ibcTxSuccessDiff: channel.ibc_tx_diff,
+              ibcTxFailed: channel.ibc_tx_failed,
+              ibcTxFailedDiff: channel.ibc_tx_failed_diff,
+              channelId: channel.channel_id,
+              zoneCounterpartyChannelId: channel.zone_counterparty_channel_id,
+              isOpened: channel.is_opened,
+            }));
+
           return {
             id: zone_counterparty,
-            counterpartyName: zone_counterparty_readable_name,
+            name: zone_counterparty_readable_name,
             zoneCounterpartyLabelUrl: zone_counterparty_label_url,
+            sourceZoneLabelUrl: sourceZone?.zone_label_url,
+            sourceZoneReadableName: sourceZone?.zone_readable_name,
             volumeIn: ibc_cashflow_in,
             volumeInDiff: ibc_cashflow_in_diff,
             volumeOut: ibc_cashflow_out,
@@ -129,13 +197,14 @@ const transform = data => {
             ibcTxSuccessDiff: ibc_tx_diff,
             ibcTxFailed: ibc_tx_failed,
             ibcTxFailedDiff: ibc_tx_failed_diff,
+            subRows: channelsFormatted,
           };
         },
       )
     : null;
 
   return {
-    channelGroup: channelsFormatted,
+    channelGroup: channelsGroupFormatted,
     sourceZone: sourceZoneFormatted,
   };
 };
