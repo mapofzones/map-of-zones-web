2d755da8be9add906ed6374723092880c3cd1d30
Maksim Tolochko <maksim.tolo@gmail.com>
2022-01-13 12:19:45 +0800

    Add success rate column

--- src/pages/ZoneDetails/components/Leaderboard/config.js
+++ src/pages/ZoneDetails/components/Leaderboard/config.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FormattedNumber } from 'react-intl';
 
-import { formatNumber } from 'common/helper';
+import { formatNumber, formatPercentage } from 'common/helper';
 
 const columns = [
   {
@@ -16,9 +16,9 @@ const columns = [
     accessor: 'volumeIn',
     id: 'volume_in',
     diffAccessor: 'volumeInDiff',
-    Cell: ({ cell }) => (
+    Cell: ({ value }) => (
       <FormattedNumber
-        value={cell.value}
+        value={value}
         style="currency"
         currency="USD"
         maximumFractionDigits="0"
@@ -31,9 +31,9 @@ const columns = [
     accessor: 'volumeOut',
     id: 'volume_out',
     diffAccessor: 'volumeOutDiff',
-    Cell: ({ cell }) => (
+    Cell: ({ value }) => (
       <FormattedNumber
-        value={cell.value}
+        value={value}
         style="currency"
         currency="USD"
         maximumFractionDigits="0"
@@ -46,7 +46,7 @@ const columns = [
     accessor: 'ibcTxSuccess',
     id: 'ibc_tx_success',
     diffAccessor: 'ibcTxSuccessDiff',
-    Cell: ({ cell }) => formatNumber(cell.value),
+    Cell: ({ value }) => formatNumber(value),
     sortDescFirst: true,
   },
   {
@@ -54,7 +54,15 @@ const columns = [
     accessor: 'ibcTxFailed',
     id: 'ibc_tx_failed',
     diffAccessor: 'ibcTxFailedDiff',
-    Cell: ({ cell }) => formatNumber(cell.value),
+    Cell: ({ value }) => formatNumber(value),
+    sortDescFirst: true,
+  },
+  {
+    Header: 'Success rate',
+    accessor: 'successRate',
+    id: 'success_rate',
+    diffAccessor: 'successRateDiff',
+    Cell: ({ value }) => formatPercentage(value),
     sortDescFirst: true,
   },
 ];
