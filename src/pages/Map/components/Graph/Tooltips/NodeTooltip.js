69e197759781a63e66ff646412aabdc08d03a3fa
Maksim Tolochko <maksim.tolo@gmail.com>
2022-02-06 21:20:49 +0800

    Node tooltip refactoring

--- src/pages/Map/components/Graph/Tooltips/NodeTooltip.js
+++ src/pages/Map/components/Graph/Tooltips/NodeTooltip.js
@@ -5,18 +5,13 @@ import { parse, stringify } from 'querystringify';
 import { FormattedNumber } from 'react-intl';
 
 import { formatNumber, formatPercentage } from 'common/helper';
+import { ReactComponent as PendingIcon } from 'assets/images/pending.svg';
 
 import styles from './index.module.css';
 
 const cx = classNames.bind(styles);
 
-function NodeTooltip({
-  node,
-  period,
-  showIbcTxsAmount,
-  showFailedIbcTxsAmount,
-  showChannels,
-}) {
+function NodeTooltip({ node, period }) {
   const history = useHistory();
   const location = useLocation();
 
@@ -83,119 +78,96 @@ function NodeTooltip({
             </div>
             <div className={cx('key-text', 'period-title')}>{period.name}</div>
           </div>
-          <div className={cx('row-tooltip')}>
-            {showChannels ? (
-              <div className={cx('col')}>
-                <div className={cx('key-text')}>Channels</div>
-                <div className={cx('item-text')}>
-                  {formatNumber(node.channels)}
-                </div>
-              </div>
-            ) : (
-              <div className={cx('col')}>
+          <div className={cx('statContainer')}>
+            <div className={cx('column')}>
+              <div className={cx('stat')}>
                 <div className={cx('key-text')}>Peers</div>
                 <div className={cx('item-text')}>
                   {formatNumber(node.peers)}
                 </div>
               </div>
-            )}
-            <div className={cx('col')}>
-              <div className={cx('key-text')}>Total TXs</div>
-              <div className={cx('item-text')}>
-                {formatNumber(node.totalTxs)}
+              <div className={cx('stat', 'secondRow')}>
+                <div className={cx('key-text')}>IBC volume</div>
+                <div className={cx('item-text')}>
+                  <FormattedNumber
+                    value={node.ibcVolume}
+                    style="currency"
+                    currency="USD"
+                    maximumFractionDigits="0"
+                  />
+                </div>
+                <div className={cx('pendingContainer')}>
+                  <PendingIcon className={cx('pendingIcon')} />
+                  <FormattedNumber
+                    value={node.ibcVolumePending}
+                    style="currency"
+                    currency="USD"
+                    maximumFractionDigits="0"
+                  />
+                </div>
               </div>
-            </div>
-          </div>
-          <div className={cx('row-tooltip')}>
-            <div className={cx('col')}>
-              <div className={cx('key-text')}>IBC transfers</div>
-              <div className={cx('item-text')}>
-                {formatNumber(node.ibcTransfers)}
-              </div>
-            </div>
-            <div className={cx('col')}>
-              <div className={cx('key-text')}>IBC share %</div>
-              <div className={cx('item-text')}>
-                {formatPercentage(node.ibcPercentage)}
-              </div>
-            </div>
-          </div>
-          <div className={cx('row-tooltip')}>
-            <div className={cx('col')}>
-              <div className={cx('key-text')}>IBC out, $</div>
-              <div className={cx('item-text')}>
-                <FormattedNumber
-                  value={node.ibcVolumeSent}
-                  style="currency"
-                  currency="USD"
-                  maximumFractionDigits="0"
-                />
-              </div>
-              <div className={cx('item-text', 'sent-title')}>
-                {formatPercentage(node.ibcVolumeSentPercentage)}
-              </div>
-            </div>
-            <div className={cx('col')}>
-              <div className={cx('key-text')}>IBC in, $</div>
-              <div className={cx('item-text')}>
-                <FormattedNumber
-                  value={node.ibcVolumeReceived}
-                  style="currency"
-                  currency="USD"
-                  maximumFractionDigits="0"
-                />
-              </div>
-              <div className={cx('item-text', 'received-title')}>
-                {formatPercentage(node.ibcVolumeReceivedPercentage)}
-              </div>
-            </div>
-          </div>
-          {showIbcTxsAmount && (
-            <div className={cx('row-tooltip')}>
-              <div className={cx('col')}>
+              <div className={cx('stat')}>
                 <div className={cx('key-text')}>IBC out, $</div>
                 <div className={cx('item-text')}>
-                  {formatNumber(node.ibcSent)}
+                  <FormattedNumber
+                    value={node.ibcVolumeSent}
+                    style="currency"
+                    currency="USD"
+                    maximumFractionDigits="0"
+                  />
                 </div>
                 <div className={cx('item-text', 'sent-title')}>
-                  {formatPercentage(node.ibcSentPercentage)}
+                  {formatPercentage(node.ibcVolumeSentPercentage)}
+                </div>
+                <div className={cx('pendingContainer')}>
+                  <PendingIcon className={cx('pendingIcon')} />
+                  <FormattedNumber
+                    value={node.ibcVolumeSentPending}
+                    style="currency"
+                    currency="USD"
+                    maximumFractionDigits="0"
+                  />
                 </div>
               </div>
-              <div className={cx('col')}>
-                <div className={cx('key-text')}>IBC in</div>
+            </div>
+            <div className={cx('column')}>
+              <div className={cx('stat')}>
+                <div className={cx('key-text')}>IBC transfers</div>
                 <div className={cx('item-text')}>
-                  {formatNumber(node.ibcReceived)}
+                  {formatNumber(node.ibcTransfers)}
+                </div>
+              </div>
+              <div className={cx('stat', 'secondRow')}>
+                <div className={cx('key-text')}>Success rate</div>
+                <div className={cx('item-text')}>
+                  {formatPercentage(node.successRate)}
+                </div>
+              </div>
+              <div className={cx('stat')}>
+                <div className={cx('key-text')}>IBC in, $</div>
+                <div className={cx('item-text')}>
+                  <FormattedNumber
+                    value={node.ibcVolumeReceived}
+                    style="currency"
+                    currency="USD"
+                    maximumFractionDigits="0"
+                  />
                 </div>
                 <div className={cx('item-text', 'received-title')}>
-                  {formatPercentage(node.ibcReceivedPercentage)}
+                  {formatPercentage(node.ibcVolumeReceivedPercentage)}
+                </div>
+                <div className={cx('pendingContainer')}>
+                  <PendingIcon className={cx('pendingIcon')} />
+                  <FormattedNumber
+                    value={node.ibcVolumeReceivedPending}
+                    style="currency"
+                    currency="USD"
+                    maximumFractionDigits="0"
+                  />
                 </div>
               </div>
             </div>
-          )}
-          {showFailedIbcTxsAmount && (
-            <div className={cx('row-tooltip')}>
-              <div className={cx('col')}>
-                <div className={cx('key-text')}>
-                  IBC Out
-                  <br />
-                  Failed
-                </div>
-                <div className={cx('item-text')}>
-                  {formatNumber(node.ibcTxOutFailed)}
-                </div>
-              </div>
-              <div className={cx('col')}>
-                <div className={cx('key-text')}>
-                  IBC in
-                  <br />
-                  Failed
-                </div>
-                <div className={cx('item-text')}>
-                  {formatNumber(node.ibcTxInFailed)}
-                </div>
-              </div>
-            </div>
-          )}
+          </div>
         </div>
 
         <button
@@ -210,10 +182,6 @@ function NodeTooltip({
   );
 }
 
-NodeTooltip.defaultProps = {
-  showChannels: false,
-  showIbcTxsAmount: false,
-  showFailedIbcTxsAmount: false,
-};
+NodeTooltip.defaultProps = {};
 
 export default NodeTooltip;
