0d0f4568eeab3d53b13e20764a0ec270e9a87ff7
sarious <sarious.w@gmail.com>
2022-05-16 16:49:30 +0400

    MOZ-330: TableCell styles refactoring

--- src/pages/Map/components/Leaderboard/TableRow/index.js
+++ src/pages/Map/components/Leaderboard/TableRow/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import styles from '../index.module.css';
 
-import TableCell from '../TableCell';
+import TableCell from '../TableCell/TableCell';
 
 const cx = classNames.bind(styles);
 
