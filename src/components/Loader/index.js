825c7390415c92ceb8aef016d755e382cf3097f4
sarious <sarious.w@gmail.com>
2022-05-27 09:48:50 +0400

    MOZ-369: show loader while react is not loaded. loader refactoring

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -6,9 +6,9 @@ const cx = classNames.bind(styles);
 function Loader() {
   return (
     <div className={cx('loader-wrapper')}>
-      <div className={cx('sk-chasing-dots')}>
-        <div className={cx('sk-child', 'sk-dot-1')} />
-        <div className={cx('sk-child', 'sk-dot-2')} />
+      <div className={cx('chasing-dots')}>
+        <div className={cx('child', 'dot-1')} />
+        <div className={cx('child', 'dot-2')} />
       </div>
     </div>
   );
