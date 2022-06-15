10f4b664f37816fcd82c85d8787878b3a24f5e2a
sarious <sarious.w@gmail.com>
2022-06-15 10:07:44 +0400

    MOZ_v2: added support of scss

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
