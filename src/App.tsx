1d02fff5d0fcaacedae6a89b5cbbf756ee6703bb
sarious <sarious.w@gmail.com>
2022-06-15 15:02:18 +0400

    MOZ_v2: fix code style

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,22 @@ import HomePage from './pages/HomePage/HomePage';
 import ZonesPage from './pages/ZonesPage/ZonesPage';
 
 function App() {
-  return (<>
-    <header>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/zones">Zones</NavLink>
-      <NavLink to="/assets">Assets</NavLink>
-      <NavLink to="/about">About</NavLink>
-    </header>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/zones" element={<ZonesPage />} />
-      <Route path="/assets" element={<AssetsPage />} />
-      <Route path="/about" element={<AboutPage />} />
-    </Routes>
-  </>);
+  return (
+    <>
+      <header>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/zones">Zones</NavLink>
+        <NavLink to="/assets">Assets</NavLink>
+        <NavLink to="/about">About</NavLink>
+      </header>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/zones" element={<ZonesPage />} />
+        <Route path="/assets" element={<AssetsPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
