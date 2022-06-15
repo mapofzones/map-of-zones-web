6a94e71d321ce24cf792ff56adaeafe7be7f4241
sarious <sarious.w@gmail.com>
2022-06-15 20:32:02 +0400

    MOZ_v2: improve main layout

--- src/layouts/Layout/Layout.tsx
+++ src/layouts/Layout/Layout.tsx
@@ -2,17 +2,19 @@ import { Outlet } from 'react-router-dom';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 
+import styles from './Layout.module.scss';
+
 const Layout = (): JSX.Element => {
   return (
-    <>
+    <div className={styles.wrapper}>
       <Header />
 
-      <div>
+      <div className={styles.body}>
         <Outlet />
       </div>
 
       <Footer />
-    </>
+    </div>
   );
 };
 
