b25f38b5516b7da6159936d211248e943af7c3bd
sarious <sarious.w@gmail.com>
2022-06-15 17:34:13 +0400

    MOZ_v2: added main layout. added header, footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,22 @@
-import { NavLink, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.scss';
+import Layout from './layouts/Layout/Layout';
 import AboutPage from './pages/AboutPage/AboutPage';
 import AssetsPage from './pages/AssetsPage/AssetsPage';
 import HomePage from './pages/HomePage/HomePage';
 import ZonesPage from './pages/ZonesPage/ZonesPage';
 
-function App() {
+const App = () => {
   return (
-    <>
-      <header>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/zones">Zones</NavLink>
-        <NavLink to="/assets">Assets</NavLink>
-        <NavLink to="/about">About</NavLink>
-      </header>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/zones" element={<ZonesPage />} />
-        <Route path="/assets" element={<AssetsPage />} />
-        <Route path="/about" element={<AboutPage />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="zones" element={<ZonesPage />} />
+        <Route path="assets" element={<AssetsPage />} />
+        <Route path="about" element={<AboutPage />} />
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
