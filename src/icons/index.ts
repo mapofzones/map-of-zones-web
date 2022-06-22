2e396637ce20c62eaa65ba06273fd1c0af059e97
sarious <sarious.w@gmail.com>
2022-06-22 16:48:26 +0400

    MOZ_v2: implement header component

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,7 +1,9 @@
+import { ReactComponent as Logo } from './logo.svg';
 import { ReactComponent as TgLogo } from './tg-logo.svg';
 import { ReactComponent as TwitterLogo } from './twitter-logo.svg';
 import { ReactComponent as GithubLogo } from './github-logo.svg';
 
+export { Logo };
 export { TgLogo };
 export { TwitterLogo };
 export { GithubLogo };
--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,12 +1,25 @@
 import { NavLink } from 'react-router-dom';
 
+import { Logo } from '../../icons';
+
+import styles from './Header.module.scss';
+
 const Header = ({ ...props }) => {
   return (
-    <header {...props}>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/zones">Zones</NavLink>
-      <NavLink to="/assets">Assets</NavLink>
-      <NavLink to="/about">About</NavLink>
+    <header className={styles.header} {...props}>
+      <Logo />
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/zones">Zones</NavLink>
+        <NavLink to="/assets">Assets</NavLink>
+        <NavLink to="/about">About</NavLink>
+      </nav>
+      <div className={styles.marketCapContainer}>
+        <span className={styles.marketCapTitle}>Cosmos Network Market Cap: </span>
+        <span className={styles.marketCapValue}>$198,308,551,250</span>
+      </div>
+      <div className={styles.tagline}>Cosmos network explorer</div>
+      <hr className={styles.line} />
     </header>
   );
 };
