444ac83009ff2ebc92bb5ca481c6c194bf48c416
sarious <sarious.w@gmail.com>
2022-06-23 15:57:32 +0400

    MOZ_v2: fix build

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,7 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
