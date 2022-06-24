22439ad130a5e6929fc9c2445a48868a17136a75
sarious <sarious.w@gmail.com>
2022-06-24 20:17:51 +0400

    MOZ_v2: added graphql queries

--- src/pages/HomePage/ZonesInfoTable/ZonesInfoTable.tsx
+++ src/pages/HomePage/ZonesInfoTable/ZonesInfoTable.tsx
@@ -1,12 +1,71 @@
+import { gql, useQuery } from '@apollo/client';
+import { useEffect, useMemo, useState } from 'react';
 import { ZoneInfoRow } from '../index';
 import styles from './ZonesInfoTable.module.scss';
 
-function ZonesInfoTable() {
+const ZONES_INFO_BY_VOLUME = gql`
+  query GetZonesInfo($period: Int!, $isMainnet: Boolean!, $isVolume: Boolean!) {
+    zonesInfo: zones_stats(
+      where: { timeframe: { _eq: $period }, is_zone_mainnet: { _eq: $isMainnet } }
+    ) {
+      id: zone
+      zoneLabelUrl: zone_label_url
+      name: zone_readable_name
+      ibcVolume: ibc_cashflow @include(if: $isVolume)
+      ibcVolumePending: ibc_cashflow_pending @include(if: $isVolume)
+      ibcVolumeRating: ibc_cashflow_rating @include(if: $isVolume)
+      ibcVolumeRatingDiff: ibc_cashflow_rating_diff @include(if: $isVolume)
+      ibcTransfers: ibc_transfers @skip(if: $isVolume)
+      ibcTransfersPending: ibc_transfers_pending @skip(if: $isVolume)
+      ibcTransfersRating: ibc_transfers_rating @skip(if: $isVolume)
+      ibcTransfersRatingDiff: ibc_transfers_rating_diff @skip(if: $isVolume)
+    }
+  }
+`;
+
+const ZONES_INFO_BY_TRANSFERS = gql`
+  query GetZonesInfo($period: Int!, $isMainnet: Boolean!) {
+    zonesInfo: zones_stats(
+      where: { timeframe: { _eq: $period }, is_zone_mainnet: { _eq: $isMainnet } }
+    ) {
+      id: zone
+      zoneLabelUrl: zone_label_url
+      name: zone_readable_name
+      ibcVolume: ibc_transfers
+      ibcVolumePending: ibc_transfers_pending
+      ibcVolumeRating: ibc_transfers_rating
+      ibcVolumeRatingDiff: ibc_transfers_rating_diff
+    }
+  }
+`;
+
+function ZonesInfoTable({ columnType }: any) {
+  console.log('ZonesInfoTable', columnType);
+  // const [zonseInfo, setZonesInfo] = useState([]);
+  const { loading, error, data } = useQuery(ZONES_INFO_BY_VOLUME, {
+    variables: { period: 24, isMainnet: true, isVolume: columnType === 'volume' },
+  });
+  // useEffect(() => {
+  //   const query = columnType === 'volume' ? ZONES_INFO_BY_VOLUME : ZONES_INFO_BY_TRANSFERS;
+  //   const { loading, error, data } = useQuery(query, {
+  //     variables: { period: 24, isMainnet: true },
+  //   });
+  //   console.log(data);
+  //   setZonesInfo(data.zonesInfo);
+  // }, [columnType]);
+
   return (
     <div className={styles.zonesInfoTable}>
-      <ZoneInfoRow data={{ name: 'Osmosis', value: '$16,150,297,401', pendingValue: '$74,546' }} />
-      <ZoneInfoRow data={{ name: 'Injective', value: '$5,273,986,485', pendingValue: '$34,236' }} />
-      <ZoneInfoRow data={{ name: 'Juno', value: '$2,920,284,678', pendingValue: '$14,465' }} />
+      {loading && <p>Loading...</p>}
+      {error && <p>Error...</p>}
+      {data &&
+        data.zonesInfo &&
+        data.zonesInfo.map((info: any) => (
+          <ZoneInfoRow
+            key={info.id}
+            data={{ name: info.name, value: info.ibcVolume, pendingValue: info.ibcVolumePending }}
+          />
+        ))}
     </div>
   );
 }
