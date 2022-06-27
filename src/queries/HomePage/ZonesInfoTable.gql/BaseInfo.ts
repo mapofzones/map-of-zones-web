43c62409c6384d00f239a4c2e9ef11e58e98befb
sarious <sarious.w@gmail.com>
2022-06-27 14:09:40 +0400

    MOZ_v2: added total info card

--- src/queries/HomePage/ZonesInfoTable.gql/BaseInfo.ts
+++ src/queries/HomePage/ZonesInfoTable.gql/BaseInfo.ts
@@ -3,7 +3,7 @@ import { gql } from '@apollo/client';
 export const BASE_INFO_FRAGMENT = gql`
   fragment BaseInfo on zones_stats {
     id: zone
-    zoneLabelUrl: zone_label_url
+    logoUrl: zone_label_url
     name: zone_readable_name
   }
 `;
