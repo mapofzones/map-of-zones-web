c2a8d60674429cf92426b616e4157de8fd0e1189
Anton Lashchotka <a.lashchotka@knomary.com>
2022-07-13 23:42:11 +0300

    zones_page: add QuestionMark and Circle icons for table headers

--- src/pages/ZonesPage/ZonesTable/TableHeader/TableHeader.tsx
+++ src/pages/ZonesPage/ZonesTable/TableHeader/TableHeader.tsx
@@ -1,3 +1,5 @@
+import { QuestionMark } from 'icons';
+
 import styles from './TableHeader.module.scss';
 
 export function TableHeader() {
@@ -6,13 +8,36 @@ export function TableHeader() {
       <div className={styles.title}>#</div>
       <div className={styles.title}>Name</div>
       <div className={styles.title}>IBC Volume</div>
-      <div className={styles.title}>IBC Volume In</div>
-      <div className={styles.title}>IBC Volume Out</div>
-      <div className={styles.title}>Total TXS</div>
-      <div className={styles.title}>IBC Transfers</div>
-      <div className={styles.title}>Peers</div>
-      <div className={styles.title}>Channels</div>
-      <div className={styles.title}>DAU</div>
+      <div className={styles.title}>
+        <div className={styles.targetCircle} />
+        IBC Volume In
+        <QuestionMark className={styles.questionMark} />
+      </div>
+      <div className={styles.title}>
+        <div className={styles.sourceCircle} />
+        IBC Volume Out
+        <QuestionMark className={styles.questionMark} />
+      </div>
+      <div className={styles.title}>
+        Total TXS
+        <QuestionMark className={styles.questionMark} />
+      </div>
+      <div className={styles.title}>
+        IBC Transfers
+        <QuestionMark className={styles.questionMark} />
+      </div>
+      <div className={styles.title}>
+        Peers
+        <QuestionMark className={styles.questionMark} />
+      </div>
+      <div className={styles.title}>
+        Channels
+        <QuestionMark className={styles.questionMark} />
+      </div>
+      <div className={styles.title}>
+        DAU
+        <QuestionMark className={styles.questionMark} />
+      </div>
       <div className={styles.title}>IBC Transfers Activity</div>
     </div>
   );
