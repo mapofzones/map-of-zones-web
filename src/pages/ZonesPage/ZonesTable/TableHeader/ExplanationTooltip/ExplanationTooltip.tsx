c5b01f76fd8e3addbebb43da1e9fd12cbe687a82
ALashchotka <flatronvin@gmail.com>
2022-07-17 22:52:58 +0300

    zones_page: TMP disable eslint warning

--- src/pages/ZonesPage/ZonesTable/TableHeader/ExplanationTooltip/ExplanationTooltip.tsx
+++ src/pages/ZonesPage/ZonesTable/TableHeader/ExplanationTooltip/ExplanationTooltip.tsx
@@ -2,6 +2,8 @@ import { QuestionMark } from 'icons';
 
 import styles from './ExplanationTooltip.module.scss';
 
+// TODO: add proptypes
+// eslint-disable-next-line react/prop-types
 export function ExplanationTooltip({ text = '' }) {
   return (
     <div className={styles.container}>
