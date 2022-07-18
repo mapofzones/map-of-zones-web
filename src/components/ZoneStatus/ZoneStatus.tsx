d24f8e85479c61a9fe63f0660800342c59eb3c36
Anton Lashchotka <a.lashchotka@knomary.com>
2022-07-19 00:40:47 +0300

    zones_page: fix ZoneStatus component typings

--- src/components/ZoneStatus/ZoneStatus.tsx
+++ src/components/ZoneStatus/ZoneStatus.tsx
@@ -1,6 +1,3 @@
-// TODO: fix lint error
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-// @ts-nocheck
 import cn from 'classnames';
 
 import { Tooltip } from 'components';
@@ -8,31 +5,34 @@ import { Tooltip } from 'components';
 import styles from './ZoneStatus.module.scss';
 import { ZoneStatusProps } from './ZoneStatus.props';
 
-const STATUS_ICON_COLOR_BY_ZONE = {
-  undefined: '#FF4455',
-  null: '#FF4455',
+const STATUS_ICON_COLOR_BY_ZONE: Record<string, string> = {
+  default: '#FF4455',
   true: '#66DD55',
   false: '#FF9900',
 };
 
-const STATUS_TITLE_BY_ZONE = {
-  undefined: 'Indirectly obtained data',
-  null: 'Indirectly obtained data',
+const STATUS_TITLE_BY_ZONE: Record<string, string> = {
+  default: 'Indirectly obtained data',
   true: 'Connected & Up to date',
   false: 'Synchronizing',
 };
 
 export function ZoneStatus({ className, status }: ZoneStatusProps) {
+  const parsedStatus = typeof status === 'boolean' ? `${status}` : 'default';
+
   return (
     <div className={styles.container}>
       <div className={cn(styles.status, className)}>
         <div
-          style={{ background: STATUS_ICON_COLOR_BY_ZONE[status] }}
+          style={{ backgroundColor: STATUS_ICON_COLOR_BY_ZONE[parsedStatus] }}
           className={styles.iconBackground}
         />
-        <div style={{ background: STATUS_ICON_COLOR_BY_ZONE[status] }} className={styles.icon} />
+        <div
+          style={{ backgroundColor: STATUS_ICON_COLOR_BY_ZONE[parsedStatus] }}
+          className={styles.icon}
+        />
       </div>
-      <Tooltip className={styles.tooltip} text={STATUS_TITLE_BY_ZONE[status]} />
+      <Tooltip className={styles.tooltip} text={STATUS_TITLE_BY_ZONE[parsedStatus]} />
     </div>
   );
 }
