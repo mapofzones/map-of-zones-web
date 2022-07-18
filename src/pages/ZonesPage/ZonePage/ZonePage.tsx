616edddf49ef81f9afb804f75f170d43822934da
Anton Lashchotka <a.lashchotka@knomary.com>
2022-07-18 04:01:29 +0300

    zones_peers: add zone page header UI

--- src/pages/ZonesPage/ZonePage/ZonePage.tsx
+++ src/pages/ZonesPage/ZonePage/ZonePage.tsx
@@ -1,16 +1,83 @@
+import cn from 'classnames';
 import { NavLink, Outlet } from 'react-router-dom';
 
-import { ButtonGroup } from 'components';
+import { ExternalLink, PeriodSelector, ZoneLogo } from 'components';
+import { EarthIcon } from 'icons';
+
+import styles from './ZonePage.module.scss';
+import { useZoneDetails } from './ZonePeers/useZoneDetails';
 
 export function ZonePage() {
+  const { data, loading } = useZoneDetails();
+
   return (
     <div>
-      <ButtonGroup>
-        <NavLink to="overview">Overview</NavLink>
-        <NavLink to="peers">Peers</NavLink>
-        <NavLink to="nodes">Nodes</NavLink>
-        <NavLink to="pools">Pools</NavLink>
-      </ButtonGroup>
+      <div className={styles.header}>
+        <div>
+          <div className={styles.detailsTitle}>
+            <ZoneLogo
+              logoUrl={data?.logoUrl}
+              name={data?.name}
+              size={'36px'}
+              loading={loading}
+              className={styles.zoneLogo}
+            />
+            <span className={styles.zoneName}>{data?.name}</span>
+          </div>
+          <span className={styles.zoneWebsite}>
+            {data?.website && (
+              <ExternalLink Icon={EarthIcon} href={data.website}>
+                {data.website}
+              </ExternalLink>
+            )}
+          </span>
+        </div>
+
+        <div className={styles.navigationButtonsContainer}>
+          <NavLink
+            className={({ isActive }) =>
+              cn(styles.navigationButton, {
+                [styles.navigationButtonActive]: isActive,
+              })
+            }
+            to="overview"
+          >
+            Overview
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              cn(styles.navigationButton, {
+                [styles.navigationButtonActive]: isActive,
+              })
+            }
+            to="peers"
+          >
+            Peers
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              cn(styles.navigationButton, {
+                [styles.navigationButtonActive]: isActive,
+              })
+            }
+            to="nodes"
+          >
+            Nodes
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => {
+              return cn(styles.navigationButton, {
+                [styles.navigationButtonActive]: isActive,
+              });
+            }}
+            to="pools"
+          >
+            Pools
+          </NavLink>
+        </div>
+
+        <PeriodSelector />
+      </div>
 
       <Outlet />
     </div>
