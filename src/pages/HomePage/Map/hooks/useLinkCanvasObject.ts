1da44eed8675e09263b87a5bc99a1205489e2b84
sarious <sarious.w@gmail.com>
2022-07-28 18:47:42 +0400

    map-2d: comet everything with same length

--- src/pages/HomePage/Map/hooks/useLinkCanvasObject.ts
+++ src/pages/HomePage/Map/hooks/useLinkCanvasObject.ts
@@ -4,6 +4,11 @@ import { LinkObject } from 'react-force-graph-2d';
 
 import { HoveredZoneKeyType, Link, SelectedZoneKeyType } from '../Types';
 
+const COMET_SPEED = 0.001;
+const COMET_LENGTH = 14;
+const ACTIVE_LINE_COLOR = '#4f4f5a';
+const NORMAL_LINE_COLOR = '#212129';
+
 function isLinkRelatedToNode(nodeKey: SelectedZoneKeyType | HoveredZoneKeyType, link: Link) {
   return !!nodeKey && (nodeKey === link.source.zone || nodeKey === link.target.zone);
 }
@@ -48,7 +53,7 @@ function drawLinkCanvasObject(
 function drawLine(ctx: CanvasRenderingContext2D, link: Link, isRalatedToActiveZone: boolean) {
   if (link.source.x && link.source.y && link.target.x && link.target.y) {
     ctx.lineWidth = 1;
-    ctx.strokeStyle = isRalatedToActiveZone ? '#4f4f5a' : '#212129';
+    ctx.strokeStyle = isRalatedToActiveZone ? ACTIVE_LINE_COLOR : NORMAL_LINE_COLOR;
 
     ctx.beginPath();
     ctx.moveTo(link.source.x, link.source.y);
@@ -58,13 +63,8 @@ function drawLine(ctx: CanvasRenderingContext2D, link: Link, isRalatedToActiveZo
 }
 
 function drawCommet(ctx: CanvasRenderingContext2D, link: any) {
-  const commetRadius = 1;
-  const commetColor = 'red';
-  const particleSpeed = 0.001;
   const { x: sourceX, y: sourceY } = link.source;
   const { x: targetX, y: targetY } = link.target;
-  const xLength = targetX - sourceX;
-  const yLength = targetY - sourceY;
 
   if (
     sourceX === undefined ||
@@ -78,30 +78,45 @@ function drawCommet(ctx: CanvasRenderingContext2D, link: any) {
   const commet = link.__commet || {};
 
   let commetProgress: number = commet.__progress ?? 0;
-  commetProgress += particleSpeed;
+  commetProgress += COMET_SPEED;
 
   if (commetProgress >= 1) {
     link.__commet = undefined;
     return;
   }
 
-  const commetPosX = sourceX + xLength * commetProgress || 0;
-  const commetPosY = sourceY + yLength * commetProgress || 0;
-  const tailOffset = commetProgress - 0.1 > 0 ? commetProgress - 0.1 : 0;
+  calculateAndDrawComet(ctx, targetX, sourceX, targetY, sourceY, commetProgress);
 
-  const tailPosX = sourceX + tailOffset * xLength;
-  const tailPosY = sourceY + tailOffset * yLength;
+  commet.__progress = commetProgress;
+  link.__commet = commet;
+}
+function calculateAndDrawComet(
+  ctx: CanvasRenderingContext2D,
+  targetX: number,
+  sourceX: number,
+  targetY: number,
+  sourceY: number,
+  commetProgress: number
+) {
+  const diffX = targetX - sourceX;
+  const diffY = targetY - sourceY;
+  const distance = Math.sqrt(diffX * diffX + diffY * diffY);
 
-  const gradient = ctx.createLinearGradient(commetPosX, commetPosY, tailPosX, tailPosY);
+  const endProgress = commetProgress - COMET_LENGTH / distance;
+  const cometEndProgress = endProgress > 0 ? endProgress : 0;
+
+  const cometStartX = sourceX + diffX * commetProgress || 0;
+  const cometStartY = sourceY + diffY * commetProgress || 0;
+  const cometEndX = sourceX + diffX * cometEndProgress || 0;
+  const cometEndY = sourceY + diffY * cometEndProgress || 0;
+
+  const gradient = ctx.createLinearGradient(cometStartX, cometStartY, cometEndX, cometEndY);
   gradient.addColorStop(0, '#FFFFFFFF');
   gradient.addColorStop(1, '#FFFFFF00');
 
   ctx.strokeStyle = gradient;
   ctx.beginPath();
-  ctx.moveTo(commetPosX, commetPosY);
-  ctx.lineTo(tailPosX, tailPosY);
+  ctx.moveTo(cometStartX, cometStartY);
+  ctx.lineTo(cometEndX, cometEndY);
   ctx.stroke();
-
-  commet.__progress = commetProgress;
-  link.__commet = commet;
 }
