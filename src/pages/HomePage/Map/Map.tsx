e255d226c9e3b147b7d51dc82cd7d12f3c95d82c
sarious <sarious.w@gmail.com>
2022-07-29 19:04:39 +0400

    map-2d: map zooming

--- src/pages/HomePage/Map/Map.tsx
+++ src/pages/HomePage/Map/Map.tsx
@@ -1,6 +1,9 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 
 import ForceGraph2D, { NodeObject } from 'react-force-graph-2d';
+
+import { Button } from 'components';
+import { ZoomIn, ZoomOut } from 'icons';
 
 import { useClearSelectedNode, useHoveredZone, useSelectedZone } from './hooks/eventHooks';
 import { useGraphData } from './hooks/useGraphData';
@@ -9,10 +12,13 @@ import { useNodeCanvasObject } from './hooks/useNodeCanvasObject';
 import styles from './Map.module.scss';
 import { Link, MapNode } from './Types';
 
+const ZOOM_VALUES = [0.75, 1, 1.5, 2.25];
+
 export function Map() {
   const [selectedZoneKey, onZoneClick] = useSelectedZone();
   const [hoveredZoneKey, onZoneHover] = useHoveredZone();
   const { graphData, loading } = useGraphData();
+  const [currentZoomIndex, setCurrentZoomIndex] = useState(1);
 
   const graphRef = useRef<any>();
   useEffect(() => {
@@ -31,9 +37,28 @@ export function Map() {
 
   const clearSelectedNode = useClearSelectedNode(selectedZoneKey);
 
-  const onZoomClick = useCallback(() => {
-    graphRef.current.zoom(2, 1000);
-  }, []);
+  useEffect(() => {
+    graphRef.current.zoom(ZOOM_VALUES[currentZoomIndex], 500);
+  }, [currentZoomIndex]);
+
+  const isZoomInDisabled = useMemo(
+    () => currentZoomIndex >= ZOOM_VALUES.length - 1,
+    [currentZoomIndex]
+  );
+
+  const isZoomOutDisabled = useMemo(() => currentZoomIndex <= 0, [currentZoomIndex]);
+
+  const onZoomIn = useCallback(() => {
+    if (!isZoomInDisabled) {
+      setCurrentZoomIndex(currentZoomIndex + 1);
+    }
+  }, [isZoomInDisabled, currentZoomIndex]);
+
+  const onZoomOut = useCallback(() => {
+    if (!isZoomOutDisabled) {
+      setCurrentZoomIndex(currentZoomIndex - 1);
+    }
+  }, [isZoomOutDisabled, currentZoomIndex]);
 
   return (
     <div className={styles.container}>
@@ -58,7 +83,20 @@ export function Map() {
         enableZoomInteraction={false}
         enableNodeDrag={false}
       />
-      <button onClick={onZoomClick}>Zoom in</button>
+      <div className={styles.leftButtonsContainer}>
+        <Button
+          Icon={ZoomIn}
+          className={styles.zoomInBtn}
+          disabled={isZoomInDisabled}
+          onClick={onZoomIn}
+        />
+        <Button
+          Icon={ZoomOut}
+          className={styles.zoomOutBtn}
+          disabled={isZoomOutDisabled}
+          onClick={onZoomOut}
+        />
+      </div>
     </div>
   );
 }
