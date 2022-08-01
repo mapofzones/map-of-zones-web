b5abffc77231f2ea7d45cd022f7f6d3616272d21
ALashchotka <flatronvin@gmail.com>
2022-08-02 01:10:09 +0300

    assets_page: add allowEmpty for RatingDiffTriangle component

--- src/components/RatingDiffTriangle/RatingDiffTriangle.tsx
+++ src/components/RatingDiffTriangle/RatingDiffTriangle.tsx
@@ -5,27 +5,38 @@ import { NumberFormat, NumberType } from 'components/ui';
 import styles from './RatingDiffTriangle.module.scss';
 
 export function RatingDiffTriangle({
+  allowEmpty = false,
   className,
   numberType = NumberType.Number,
   ratingDiff,
 }: {
+  allowEmpty?: boolean;
   className?: string;
   numberType?: NumberType;
   ratingDiff?: number | null;
 }): JSX.Element {
-  if (!ratingDiff) {
+  if (!allowEmpty && !ratingDiff) {
     return <></>;
   }
 
   return (
-    <div className={cn(styles.ratingDiff, { [styles.negative]: ratingDiff < 0 }, className)}>
+    <div
+      className={cn(
+        styles.ratingDiff,
+        {
+          [styles.negative]: (ratingDiff as number) < 0,
+          [styles.positive]: (ratingDiff as number) > 0,
+        },
+        className
+      )}
+    >
       <div
         className={cn(styles.triangle, {
-          [styles.triangleUp]: ratingDiff > 0,
-          [styles.triangleDown]: ratingDiff < 0,
+          [styles.up]: (ratingDiff as number) > 0,
+          [styles.down]: (ratingDiff as number) < 0,
         })}
       />
-      <NumberFormat value={Math.abs(ratingDiff)} numberType={numberType} />
+      <NumberFormat value={Math.abs(ratingDiff as number)} numberType={numberType} />
     </div>
   );
 }
