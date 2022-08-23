b3e0e3fd256a9f0cb361a5cf8946fe03ace70380
ALashchotka <flatronvin@gmail.com>
2022-08-23 15:24:02 +0300

    page_width: set max page width only for header, footer and screens with table

--- src/pages/AssetsPage/AssetsPage.tsx
+++ src/pages/AssetsPage/AssetsPage.tsx
@@ -1,15 +1,16 @@
 import { ScrollUpButton } from 'components';
 
+import styles from './AssetsPage.module.scss';
 import { AssetsTable } from './AssetsTable/AssetsTable';
 import { AssetsTotalInfo } from './AssetsTotalInfo/AssetsTotalInfo';
 
 export function AssetsPage(): JSX.Element {
   return (
-    <>
+    <div className={styles.container}>
       <AssetsTotalInfo />
       <AssetsTable />
 
       <ScrollUpButton />
-    </>
+    </div>
   );
 }
