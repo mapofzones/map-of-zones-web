4e19e7977e17facb475c524f9925e31a1e926288
ALashchotka <flatronvin@gmail.com>
2022-09-05 12:43:49 +0300

    adaptive: fix useMediaQuery hook

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -11,31 +11,22 @@ export function useMediaQuery(query: string): boolean {
 
   const [matches, setMatches] = useState<boolean>(getMatches(query));
 
-  function handleChange() {
-    setMatches(getMatches(query));
-  }
-
   useEffect(() => {
+    function handleChange() {
+      setMatches(getMatches(query));
+    }
+
     const matchMedia = window.matchMedia(query);
 
     // Triggered at the first client-side load and if query changes
     handleChange();
 
     // Listen matchMedia
-    if (matchMedia.addListener) {
-      matchMedia.addListener(handleChange);
-    } else {
-      matchMedia.addEventListener('change', handleChange);
-    }
+    matchMedia.addEventListener('change', handleChange);
 
     return () => {
-      if (matchMedia.removeListener) {
-        matchMedia.removeListener(handleChange);
-      } else {
-        matchMedia.removeEventListener('change', handleChange);
-      }
+      matchMedia.removeEventListener('change', handleChange);
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [query]);
 
   return matches;
