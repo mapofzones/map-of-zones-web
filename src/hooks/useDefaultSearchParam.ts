ce70bb271fec6f2346d0163cf08d885c3368dfe0
sarious <sarious.w@gmail.com>
2022-09-09 20:09:41 +0400

    MOZ_v2: fix problem with back button (routing issue with default value)

--- src/hooks/useDefaultSearchParam.ts
+++ src/hooks/useDefaultSearchParam.ts
@@ -1,24 +1,34 @@
 import { useCallback, useEffect } from 'react';
 
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams, useLocation, useNavigate } from 'react-router-dom';
 
 export function useDefaultSearchParam<T extends string>(key: string, defaultValue: T) {
-  const [search, setSearch] = useSearchParams();
+  const [search] = useSearchParams();
+  const navigate = useNavigate();
+  const location = useLocation();
   const columnKey = search.get(key);
 
   const selectedColumnKey: T = (columnKey as T) ?? defaultValue;
 
   const setSelectedColumnKey = useCallback(
-    (value: T) => {
+    (value: T, replace = false) => {
       search.set(key, value);
-      setSearch(search);
+      navigate(
+        {
+          pathname: location.pathname,
+          search: '?' + search.toString(),
+        },
+        {
+          replace,
+        }
+      );
     },
-    [search, key, setSearch]
+    [search, key, navigate, location.pathname]
   );
 
   useEffect(() => {
     if (!search.get(key)) {
-      setSelectedColumnKey(defaultValue);
+      setSelectedColumnKey(defaultValue, true);
     }
   }, [search, key, defaultValue, setSelectedColumnKey]);
 
