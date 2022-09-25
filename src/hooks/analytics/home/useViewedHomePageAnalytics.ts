4074f0b01ef1099fdf210699b6acb117b1ea406a
sarious <sarious.w@gmail.com>
2022-09-26 00:47:56 +0400

    MOZ_v2: fix source of viewed page

--- src/hooks/analytics/home/useViewedHomePageAnalytics.ts
+++ src/hooks/analytics/home/useViewedHomePageAnalytics.ts
@@ -4,8 +4,8 @@ import { ColumnKeys } from 'pages/HomePage/Types';
 
 import { Page, PAGE_TITLE } from '../Types';
 import { trackEvent } from '../useAnalytics';
-import { getSimplePageSource } from '../utils';
 
+type HomePageSource = 'direct link' | 'share link' | 'menu';
 type HomePageParam = 'ibc volume' | 'total txs' | 'ibc transfers' | 'active users';
 
 export const HOME_PAGE_TABLE_COLUMN_TITLE: Record<ColumnKeys, HomePageParam> = {
@@ -19,7 +19,7 @@ export function useViewedHomePageAnalytics(currentPage: Page, prevPage: Page) {
   useEffect(() => {
     if (currentPage?.title !== PAGE_TITLE.Home) return;
 
-    const source = getSimplePageSource(prevPage, currentPage);
+    const source = getHomePageSource(prevPage, currentPage);
     if (source) {
       trackEvent('viewed home page', {
         referrer_url: prevPage?.title,
@@ -31,4 +31,16 @@ export function useViewedHomePageAnalytics(currentPage: Page, prevPage: Page) {
       });
     }
   }, [currentPage, prevPage]);
+}
+
+function getHomePageSource(prevPage: Page, currentPage: Page): HomePageSource | undefined {
+  if (!prevPage) {
+    if (currentPage.search.utm_source) {
+      return 'share link';
+    }
+    return 'direct link';
+  } else if (prevPage.title !== currentPage.title) {
+    return 'menu';
+  }
+  return undefined;
 }
