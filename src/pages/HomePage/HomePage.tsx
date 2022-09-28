1a29e793e8ab857ad410e6ef1e34a22e69accfbb
sarious <sarious.w@gmail.com>
2022-09-28 17:22:34 +0400

    MOZ_v2: get rid of map rendering on small screen sizes

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from 'react-router-dom';
 
+import { useTabletSmallMediaQuery } from 'hooks/useMediaQuery';
+
 import styles from './HomePage.module.scss';
 import { Map } from './Map/Map';
 
 export function HomePage() {
+  const isTableSmall = useTabletSmallMediaQuery();
+
   return (
     <div className={styles.pageContainer}>
-      <Map className={styles.map} />
+      {!isTableSmall && <Map className={styles.map} />}
       <Outlet />
     </div>
   );
