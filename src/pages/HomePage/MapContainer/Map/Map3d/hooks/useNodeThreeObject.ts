a67cb7575b0a57dbb69bdaab1313bfc4112573cd
sarious <sarious.w@gmail.com>
2022-11-24 12:16:15 +0400

    map3d: added logos

--- src/pages/HomePage/MapContainer/Map/Map3d/hooks/useNodeThreeObject.ts
+++ src/pages/HomePage/MapContainer/Map/Map3d/hooks/useNodeThreeObject.ts
@@ -1,6 +1,16 @@
 import { useCallback } from 'react';
 
-import { Scene, SphereGeometry, MeshLambertMaterial, Mesh } from 'three';
+import THREE, {
+  Scene,
+  SphereGeometry,
+  MeshLambertMaterial,
+  Mesh,
+  Group,
+  TextureLoader,
+  SpriteMaterial,
+  Sprite,
+  Cache,
+} from 'three';
 import SpriteText from 'three-spritetext';
 
 export function useNodeThreeObject(focusedNodeKey: any, focusedNodeNeighbors: Set<string>) {
@@ -11,14 +21,21 @@ export function useNodeThreeObject(focusedNodeKey: any, focusedNodeNeighbors: Se
         return scene;
       }
 
-      const sphereGeometry = new SphereGeometry(node.radius, 20, 20);
-      const sphereMaterial = new MeshLambertMaterial({
-        color: node.color,
-        opacity: 0.7,
-        transparent: true,
-      });
-      const sphere = new Mesh(sphereGeometry, sphereMaterial);
-      scene.add(sphere);
+      // const sphereGeometry = new SphereGeometry(node.radius, 20, 20);
+      // const sphereMaterial = new MeshLambertMaterial({
+      //   color: node.color,
+      //   opacity: 0.7,
+      //   transparent: true,
+      // });
+      // const sphere = new Mesh(sphereGeometry, sphereMaterial);
+      // scene.add(sphere);
+      Cache.enabled = true;
+      const logoRadiusWithScale = 25;
+      const map = new TextureLoader().load(node.logoUrl);
+      const material = new SpriteMaterial({ map: map, color: 0xffffff });
+      const sprite = new Sprite(material);
+      sprite.scale.set(logoRadiusWithScale, logoRadiusWithScale, logoRadiusWithScale);
+      scene.add(sprite);
 
       const text = new SpriteText(node.name);
       text.color = node.color;
