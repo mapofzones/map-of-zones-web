54e68e517508e933b1b52445b3e2cc0dcef84610
sarious <sarious.w@gmail.com>
2022-12-19 11:53:44 +0400

    map3d: don't draw black circle. text color and refactoring

--- src/pages/HomePage/MapContainer/Map/Map3d/utils/drawNode3d.tsx
+++ src/pages/HomePage/MapContainer/Map/Map3d/utils/drawNode3d.tsx
@@ -34,11 +34,6 @@ export function drawNode3d(node: any, images: ImagesMap) {
   ctx.stroke();
 
   circle.r *= 0.87;
-  ctx.fillStyle = 'black';
-  drawCircle(ctx, circle);
-  ctx.shadowColor = null as any;
-  ctx.shadowBlur = null as any;
-
   if (images[node.logoUrl]) {
     ctx.globalCompositeOperation = 'source-over';
     const borderR = node.radius * 0.2;
@@ -71,24 +66,24 @@ export function drawNode3d(node: any, images: ImagesMap) {
     ctx.globalAlpha = 1;
   }
 
-  drawTitle(ctx, fontSize, node, canvas, radius);
+  drawTitle(ctx, node, fontSize, radius, canvas.width / 2);
 
   return canvas;
 }
 
 function drawTitle(
   ctx: CanvasRenderingContext2D,
-  fontSize: number,
   node: any,
-  canvas: HTMLCanvasElement,
-  radius: number
+  fontSize: number,
+  radius: number,
+  centerX: number
 ) {
   ctx.textAlign = 'center';
   ctx.textBaseline = 'middle';
 
   ctx.font = `${400} ${fontSize}px Roboto`;
-  ctx.fillStyle = '#FFFFFF';
-  ctx.fillText(node.name, canvas.width / 2, radius * 2 + fontSize / 2);
+  ctx.fillStyle = '#9F9FA5';
+  ctx.fillText(node.name, centerX, radius * 2 + fontSize / 2 + 3);
 }
 
 function drawCircle(ctx: CanvasRenderingContext2D, c: any) {
