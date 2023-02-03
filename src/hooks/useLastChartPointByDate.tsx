135853cfd2357e7cd9a9dbea83f58222ba3ab2de
sarious <sarious.w@gmail.com>
2023-02-03 22:12:26 +0400

    charts: fix problem with delegates chart

--- src/hooks/useLastChartPointByDate.tsx
+++ src/hooks/useLastChartPointByDate.tsx
@@ -2,10 +2,18 @@ import { useMemo } from 'react';
 
 import moment from 'moment';
 
+import { PeriodKeys } from 'components';
 import { ChartItemWithTime } from 'types/chart';
 
-export function useLastChartPointByDate<T extends ChartItemWithTime>(data: T[]) {
+export function useLastChartPointByDate<T extends ChartItemWithTime>(
+  data: T[],
+  period: PeriodKeys
+) {
   return useMemo(() => {
+    if (period === PeriodKeys.DAY) {
+      return data;
+    }
+
     const chartMapData = data.reduce((dict: { [key: string]: T }, curr: T) => {
       const currItemTime = moment.unix(curr.time).format('DD/MM/YYYY');
 
--- src/pages/ZonesPage/ZonePage/ZoneOverview/ZoneOverviewUniqueDelegates/ZoneOverviewUniqueDelegates.tsx
+++ src/pages/ZonesPage/ZonePage/ZoneOverview/ZoneOverviewUniqueDelegates/ZoneOverviewUniqueDelegates.tsx
@@ -1,5 +1,6 @@
 import { OverviewChartCard } from 'components/OverviewChartCard';
 import { useLastChartPointByDate } from 'hooks/useLastChartPointByDate';
+import { useSelectedPeriod } from 'hooks/useSelectedPeriod';
 
 import { useZoneOverviewUniqueDelegates } from './useZoneOverviewUniqueDelegates';
 import { UNIQUE_DELEGATES_CARD_METADATA } from './ZoneOverviewUniqueDelegates.metadata';
@@ -7,9 +8,10 @@ import { UNIQUE_DELEGATES_CARD_METADATA } from './ZoneOverviewUniqueDelegates.me
 import { ZoneOverviewUniqueDelegatesProps } from '.';
 
 export function ZoneOverviewUniqueDelegates({ className }: ZoneOverviewUniqueDelegatesProps) {
+  const [period] = useSelectedPeriod();
   const { data, loading } = useZoneOverviewUniqueDelegates();
 
-  const chartData = useLastChartPointByDate(data?.chart ?? []);
+  const chartData = useLastChartPointByDate(data?.chart ?? [], period);
 
   return (
     <OverviewChartCard
