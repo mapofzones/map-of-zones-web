d136995e331418cb036aced8932cc3f25af56c93
sarious <sarious.w@gmail.com>
2023-02-22 14:46:18 +0400

    overview: use adaptive layout for different screen sizes

--- src/pages/ZonesPage/ZonePage/ZoneOverview/ZoneOverviewActivity/ZoneOverviewActivity.tsx
+++ src/pages/ZonesPage/ZonePage/ZoneOverview/ZoneOverviewActivity/ZoneOverviewActivity.tsx
@@ -47,86 +47,94 @@ export function ZoneOverviewActivity({ className }: { className?: string }) {
         )}
       </div>
       <div className={styles.valuesContainer}>
-        <ValueWithPending
-          className={cn(styles.valueBlock, styles.ibcVolume)}
-          title={'IBC Volume'}
-          value={data?.ibcVolume}
-          numberType={NumberType.Currency}
-          tooltipText={tooltips['ibcVolume']()}
-          size={ElementSize.LARGE}
-          loading={loading}
-          defaultSkeletonText={'$16 256 000'}
-        />
-
-        <div className={styles.volumeInOutContainer}>
-          <VolumeLineChart
-            className={styles.volumeLineChart}
-            volumeInPercent={data?.ibcVolumeInPercent}
-            volumeOutPercent={data?.ibcVolumeOutPercent}
+        <div className={styles.valueGroup}>
+          <ValueWithPending
+            className={cn(styles.valueBlock, styles.ibcVolume)}
+            title={'IBC Volume'}
+            value={data?.ibcVolume}
+            numberType={NumberType.Currency}
+            tooltipText={tooltips['ibcVolume']()}
+            size={ElementSize.LARGE}
+            loading={loading}
+            defaultSkeletonText={'$16 256 000'}
           />
 
-          <SkeletonTextWrapper
-            className={styles.volumeInValue}
-            loading={loading}
-            defaultText={'$7 940 600'}
-          >
-            <NumberFormat value={data?.ibcVolumeIn} numberType={NumberType.Currency} />
-          </SkeletonTextWrapper>
+          <div className={cn(styles.volumeInOutContainer, styles.valueBlock)}>
+            <VolumeLineChart
+              className={styles.volumeLineChart}
+              volumeInPercent={data?.ibcVolumeInPercent}
+              volumeOutPercent={data?.ibcVolumeOutPercent}
+            />
 
-          <SkeletonTextWrapper
-            className={cn(styles.volumeOutValue)}
-            loading={loading}
-            defaultText={'$7 940 600'}
-          >
-            <NumberFormat value={data?.ibcVolumeOut} numberType={NumberType.Currency} />
-          </SkeletonTextWrapper>
+            <div className={styles.volumeInOutValuesContainer}>
+              <SkeletonTextWrapper
+                className={styles.volumeInValue}
+                loading={loading}
+                defaultText={'$7 940 600'}
+              >
+                <NumberFormat value={data?.ibcVolumeIn} numberType={NumberType.Currency} />
+              </SkeletonTextWrapper>
+
+              <SkeletonTextWrapper
+                className={cn(styles.volumeOutValue)}
+                loading={loading}
+                defaultText={'$7 940 600'}
+              >
+                <NumberFormat value={data?.ibcVolumeOut} numberType={NumberType.Currency} />
+              </SkeletonTextWrapper>
+            </div>
+          </div>
         </div>
 
         <Divider />
 
-        <ValueWithPending
-          className={cn(styles.valueBlock, styles.transactions)}
-          title={'Transactions'}
-          value={data?.totalTxs}
-          numberType={NumberType.Number}
-          tooltipText={tooltips['totalTxs']()}
-          size={ElementSize.LARGE}
-          loading={loading}
-          defaultSkeletonText={'149 650'}
-        />
-        <ValueWithPending
-          className={styles.valueBlock}
-          title={'IBC Transfers'}
-          value={data?.ibcTransfers}
-          numberType={NumberType.Number}
-          tooltipText={tooltips['ibcTransfers']()}
-          size={ElementSize.LARGE}
-          loading={loading}
-          defaultSkeletonText={'29 848'}
-        />
+        <div className={styles.valueGroup}>
+          <ValueWithPending
+            className={cn(styles.valueBlock, styles.transactions)}
+            title={'Transactions'}
+            value={data?.totalTxs}
+            numberType={NumberType.Number}
+            tooltipText={tooltips['totalTxs']()}
+            size={ElementSize.LARGE}
+            loading={loading}
+            defaultSkeletonText={'149 650'}
+          />
+          <ValueWithPending
+            className={styles.valueBlock}
+            title={'IBC Transfers'}
+            value={data?.ibcTransfers}
+            numberType={NumberType.Number}
+            tooltipText={tooltips['ibcTransfers']()}
+            size={ElementSize.LARGE}
+            loading={loading}
+            defaultSkeletonText={'29 848'}
+          />
+        </div>
 
         <Divider />
 
-        <ValueWithPending
-          className={cn(styles.valueBlock, styles.activeAddresses)}
-          title={'Active Addresses'}
-          value={data?.dau}
-          numberType={NumberType.Number}
-          tooltipText={tooltips['dau'](selectedPeriod)}
-          size={ElementSize.LARGE}
-          loading={loading}
-          defaultSkeletonText={'17 000'}
-        />
-        <ValueWithPending
-          className={styles.valueBlock}
-          title={'Active IBC Transfers'}
-          value={data?.ibcDau}
-          numberType={NumberType.Number}
-          tooltipText={tooltips['ibcDau'](selectedPeriod)}
-          size={ElementSize.LARGE}
-          loading={loading}
-          defaultSkeletonText={'5 000'}
-        />
+        <div className={styles.valueGroup}>
+          <ValueWithPending
+            className={cn(styles.valueBlock, styles.activeAddresses)}
+            title={'Active Addresses'}
+            value={data?.dau}
+            numberType={NumberType.Number}
+            tooltipText={tooltips['dau'](selectedPeriod)}
+            size={ElementSize.LARGE}
+            loading={loading}
+            defaultSkeletonText={'17 000'}
+          />
+          <ValueWithPending
+            className={styles.valueBlock}
+            title={'Active IBC Transfers'}
+            value={data?.ibcDau}
+            numberType={NumberType.Number}
+            tooltipText={tooltips['ibcDau'](selectedPeriod)}
+            size={ElementSize.LARGE}
+            loading={loading}
+            defaultSkeletonText={'5 000'}
+          />
+        </div>
       </div>
     </Card>
   );
