3f8f6bc91828dcf9495627371de6dd2b0cf55f9a
sarious <sarious.w@gmail.com>
2023-03-01 21:00:22 +0400

    swap: added styles to widget

--- src/pages/SwapPage/SwapPage.tsx
+++ src/pages/SwapPage/SwapPage.tsx
@@ -6,15 +6,35 @@ import styles from './SwapPage.module.scss';
 import { SwapPageProps } from '.';
 
 export function SwapPage({ className, ...props }: SwapPageProps) {
-  console.log('swap page');
   return (
     <div className={cn(styles.container, className)} {...props}>
-      <SquidWidget
-        config={{
-          companyName: 'Some company',
-          slippage: 3,
-        }}
-      />
+      <div>
+        <SquidWidget
+          config={{
+            companyName: 'MOZ',
+            slippage: 1.5,
+            style: {
+              neutralContent: '#8f8f96',
+              baseContent: '#e1e0e0',
+              base100: '#33333d',
+              base200: '#1c1c25',
+              base300: '#15151e',
+              error: '#ff4455',
+              warning: '#ff9900',
+              success: '#66dd55',
+              primary: '#ee11cc',
+              secondary: '#37394c',
+              secondaryContent: '#b2bcd3',
+              neutral: '#2a2a33',
+              roundedBtn: '5px',
+              roundedBox: '5px',
+              roundedDropDown: '5px',
+              displayDivider: false,
+            },
+            mainLogoUrl: '',
+          }}
+        />
+      </div>
     </div>
   );
 }
