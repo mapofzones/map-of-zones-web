1197224e2d0ca3fee21442eae39ab880ed1a3093
sarious <sarious.w@gmail.com>
2023-05-01 19:29:11 +0400

    refactoring: don't rerender all page when change search zone value

--- src/pages/HomePage/Sidebar/ZonesInfo/ZoneTitleSearchProvider.tsx
+++ src/pages/HomePage/Sidebar/ZonesInfo/ZoneTitleSearchProvider.tsx
@@ -1,6 +1,6 @@
-import { ReactNode, createContext, useContext } from 'react';
+import { ReactNode, createContext, useContext, useEffect, useState } from 'react';
 
-import { useDefaultSearchParam } from 'hooks/useDefaultSearchParam';
+import { useSearchParams } from 'react-router-dom';
 
 interface SearchApi {
   setSearchValue: (value: string) => void;
@@ -12,8 +12,21 @@ const SearchContext = createContext<string>('');
 export const useSearchApiState = () => useContext(SearchApiContext);
 export const useSearchState = () => useContext(SearchContext);
 
+const SEARCH_KEY = 'searchZone';
+
 export function ZoneTitleSearchProvider({ children }: { children: ReactNode }) {
-  const [searchValue, setSearchValue] = useDefaultSearchParam('searchZone');
+  const [search] = useSearchParams();
+  const [searchValue, setSearchValue] = useState(search.get(SEARCH_KEY) ?? '');
+
+  useEffect(() => {
+    const url = new URL(window.location.toString());
+    if (searchValue) {
+      url.searchParams.set(SEARCH_KEY, searchValue);
+    } else {
+      url.searchParams.delete(SEARCH_KEY);
+    }
+    window.history.pushState(null, '', url.toString());
+  }, [searchValue]);
 
   return (
     <SearchApiContext.Provider value={{ setSearchValue }}>
--- src/utils/dateTimeUtils.ts
+++ src/utils/dateTimeUtils.ts
@@ -1,5 +1,7 @@
 import moment from 'moment';
 
+// TODO: use dayjs instead of moment
+
 export function formatUnixUTC(value: number, timeFormat: string) {
   return moment.unix(value).utc().format(timeFormat);
 }
