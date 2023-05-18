1a743e06ec21416dc77d0f197de548a697aab432
sarious <sarious.w@gmail.com>
2023-05-18 12:54:45 +0400

    button: fix button

--- src/pages/HomePage/Sidebar/ShowDetailsButton/ShowDetailsButton.tsx
+++ src/pages/HomePage/Sidebar/ShowDetailsButton/ShowDetailsButton.tsx
@@ -6,9 +6,13 @@ import { Button } from 'ui';
 import styles from './ShowDetailsButton.module.scss';
 import ShowDetailsButtonProps from './ShowDetailsButton.props';
 
-export function ShowDetailsButton({ title, primary = false }: ShowDetailsButtonProps): JSX.Element {
+export function ShowDetailsButton({
+  title,
+  primary = false,
+  onClick,
+}: ShowDetailsButtonProps): JSX.Element {
   return (
-    <Button className={cn(styles.wrapper, { [styles.primary]: primary })}>
+    <Button className={cn(styles.wrapper, { [styles.primary]: primary })} onClick={onClick}>
       <div className={styles.content}>
         <span className={styles.btnText}>{title}</span>
         <ArrowInCircle className={styles.arrowIcon} />
