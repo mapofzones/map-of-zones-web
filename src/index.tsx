f0411103b2dbd0c8258609abe47932403c8f22f6
sarious <sarious.w@gmail.com>
2023-05-24 16:37:34 +0400

    refactoring Apollo provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,26 @@
 import React from 'react';
 
-import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client';
 import ReactDOM from 'react-dom/client';
 import ReactModal from 'react-modal';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.scss';
+import { GraphQLProvider } from 'providers/GraphQLProvider';
 import { RestApiQueryProvider } from 'providers/RestApiQueryProvider';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-const link = new HttpLink({ uri: process.env.REACT_APP_GRAPHQL_HTTP_URI });
-
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: link,
-});
 
 const rootHtmlElement = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(rootHtmlElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ApolloProvider client={client}>
+      <GraphQLProvider>
         <RestApiQueryProvider>
           <App />
         </RestApiQueryProvider>
-      </ApolloProvider>
+      </GraphQLProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
