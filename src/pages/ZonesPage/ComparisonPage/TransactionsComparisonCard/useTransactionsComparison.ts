80f50900d48f20e9dae2503c8b4ebf367a91b12c
sarious <sarious.w@gmail.com>
2023-06-12 10:15:38 +0400

    comparison: fix retrieving data for Transactions card

--- src/pages/ZonesPage/ComparisonPage/TransactionsComparisonCard/useTransactionsComparison.ts
+++ src/pages/ZonesPage/ComparisonPage/TransactionsComparisonCard/useTransactionsComparison.ts
@@ -1,5 +1,3 @@
-import { useEffect, useState } from 'react';
-
 import { useQueries } from '@tanstack/react-query';
 
 import { OVERVIEW_PERIODS_API_KEYS } from 'components/OverviewChartCardWithMetadata';
@@ -22,10 +20,6 @@ export function useTransactionsComparison(
   zones: string[],
   period: AnalysisCardPeriod
 ): ZonesTransactionsComparisonResult {
-  const [handledData, setHandledData] = useState<ZonesTransactionsComparisonResult>(
-    {} as ZonesTransactionsComparisonResult
-  );
-
   const queries = zones.map((zone) => {
     return {
       queryKey: [`txsChart/${zone}/${OVERVIEW_PERIODS_API_KEYS[period]}`],
@@ -34,19 +28,21 @@ export function useTransactionsComparison(
   });
   const responses = useQueries<TransactionApiResult[]>({ queries });
 
-  useEffect(() => {
-    const result = responses.map((result) => (result.data as any)?.data).filter((d) => !!d) ?? [];
-    const loading = responses.every((result) => result.isLoading);
-    const charts = mapCharts(result, ['chart'], 'txsCount');
+  const result =
+    responses.map((result) => (result.data as TransactionRootApiResult)?.data).filter((d) => !!d) ??
+    [];
+  const loading = responses.every((result) => result.isLoading);
+  const charts = mapCharts(result, ['chart'], 'txsCount');
 
-    setHandledData({
-      data: result.map((item) => ({ zone: item.zone, totalTxsCount: item.totalTxsCount })),
-      charts,
-      loading,
-    });
-  }, [JSON.stringify(responses)]); // fix bug in @tanstack/react-query library: responses has new reference for each rerender, so effect invoked too often
+  return {
+    data: result.map((item) => ({ zone: item.zone, totalTxsCount: item.totalTxsCount })),
+    charts,
+    loading,
+  };
+}
 
-  return handledData;
+interface TransactionRootApiResult {
+  data: TransactionApiResult;
 }
 
 interface TransactionApiResult {
